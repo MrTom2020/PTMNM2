50c6028375df956b46d61fd2d4d9759578cbfb72
MrTom2020 <tomhumchin2@gmail.com>
2021-06-23 16:10:38 +0700

    make it better

---
+++ MVC/view/pages/html/menu_main/congdong.php
@@ -0,0 +1,44 @@
+<form  action="../tienich/ti" method="POST">
+<div class="max-width text-center middle-things" style="margin-top:15vh;">
+<div class="row">
+<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn" style="margin-bottom:5vh;">
+<div class="animated features-ml">
+<img src="https://img.icons8.com/fluent/71/26e07f/calculator.png"/>
+<h3 class="fourthlabel">Sổ Chi</h3>
+<input type="submit" class="form-control"  name="mt" id="useName" value="Một công cụ ghi chép tiền xuất.">
+</div>
+</div>
+<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
+<div class="animated features-ml">
+<img src="https://img.icons8.com/fluent/71/26e07f/estimate.png"/>
+<h3 class="fourthlabel">Sổ Thu</h3>
+<input type="submit" class="form-control"  name="mt" id="useName" value="Một công cụ ghi chép tiền vào.">
+</div>
+</div>
+<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
+<div class="animated features-ml">
+<img src="https://img.icons8.com/fluent/71/26e07f/rupee-exchange.png"/>
+<h3 class="fourthlabel">Sổ đầu tư</h3>
+<input type="submit" class="form-control"  name="cd" id="cd" value="Một công cụ ghi chép tiền đầu tư.">
+</div>
+</div>
+
+
+<div class="col-md-4 col-lg-4  col-xs-6 animated fadeIn" style="margin-bottom:5vh;">
+<div class="animated features-ml">
+<img src="https://img.icons8.com/dotty/71/26e07f/planner.png"/>
+<h3 class="fourthlabel">Sổ kế hoạch</h3>
+<input type="submit" class="form-control"  name="mt" id="useName" value="Một công cụ ghi chép kế hoạch.">
+</div>
+</div>
+<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
+<div class="animated features-ml">
+<img src="https://img.icons8.com/pastel-glyph/71/26e07f/note.png"/>
+<h3 class="fourthlabel">Sổ Nợ</h3>
+<input type="submit" class="form-control"  name="mt" id="useName" value="Một công cụ ghi chép tiền nợ">
+</div>
+</div>
+
+</div>
+</div>
+</form>
