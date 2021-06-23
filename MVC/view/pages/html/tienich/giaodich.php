66f61c1050572a97e92b6c112d9d6de1a5331f60
MrTom2020 <tomhumchin2@gmail.com>
2021-06-23 15:09:07 +0700

    make it better

--- MVC/view/pages/html/tienich/giaodich.php
+++ MVC/view/pages/html/tienich/giaodich.php
@@ -5,21 +5,21 @@
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn" style="margin-bottom:5vh;">
 <div class="animated features-ml">
 <img src="https://img.icons8.com/fluent/71/26e07f/calculator.png"/>
-<h3 class="fourthlabel">Amazing calculator</h3>
+<h3 class="fourthlabel">Sổ Chi</h3>
 <input type="submit" class="form-control"  name="mt" id="useName" value="Amazing calculator là một công cụ tính toán tuyệt vời.">
 </div>
 </div>
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
 <div class="animated features-ml">
 <img src="https://img.icons8.com/fluent/71/26e07f/estimate.png"/>
-<h3 class="fourthlabel">Amazing split the money</h3>
+<h3 class="fourthlabel">Sổ Thu</h3>
 <input type="submit" class="form-control"  name="mt" id="useName" value="Amazing split the money là công cụ chia tiền hữu ích tiền.">
 </div>
 </div>
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
 <div class="animated features-ml">
 <img src="https://img.icons8.com/fluent/71/26e07f/rupee-exchange.png"/>
-<h3 class="fourthlabel">Amazing look up</h3>
+<h3 class="fourthlabel">Sổ đầu tư</h3>
 <input type="submit" class="form-control"  name="cd" id="cd" value="Amazing money conversion là công cụ chuyển đổi dv tiền.">
 </div>
 </div>
@@ -28,46 +28,18 @@
 <div class="col-md-4 col-lg-4  col-xs-6 animated fadeIn" style="margin-bottom:5vh;">
 <div class="animated features-ml">
 <img src="https://img.icons8.com/dotty/71/26e07f/planner.png"/>
-<h3 class="fourthlabel">Amazing plan</h3>
+<h3 class="fourthlabel">Sổ kế hoạch</h3>
 <input type="submit" class="form-control"  name="mt" id="useName" value="Amazing plan giúp bạn lập kế hoạch dễ dàng.">
 </div>
 </div>
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
 <div class="animated features-ml">
 <img src="https://img.icons8.com/pastel-glyph/71/26e07f/note.png"/>
-<h3 class="fourthlabel">Amazing note</h3>
+<h3 class="fourthlabel">Sổ Nợ</h3>
 <input type="submit" class="form-control"  name="mt" id="useName" value="Amazing loan calculation sẽ giúp bạn ghi nhớ các khoản vay.">
 </div>
 </div>
-<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
-<div class="animated features-ml">
-<img src="https://img.icons8.com/fluent/71/26e07f/growing-money.png"/>
-<h3 class="fourthlabel">Amazing plan money</h3>
-<input type="submit" class="form-control"  name="mt" id="useName" value="Amazing loan calculation sẽ giúp bạn tính các khoản.">
-</div>
-</div>
 
-<div class="col-md-4 col-lg-4  col-xs-6 animated fadeIn" style="margin-bottom:5vh;">
-<div class="animated features-ml">
-<img src="https://img.icons8.com/fluent/71/26e07f/apple-music.png"/>
-<h3 class="fourthlabel">Amazing music</h3>
-<input type="submit" class="form-control"  name="nhac" id="nhac" value="Amazing music sẽ giúp bạn thư giãn.">
-</div>
-</div>
-<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
-<div class="animated features-ml">
-<img src="https://img.icons8.com/fluent/72/26e07f/microsoft-excel-2019.png"/>
-<h3 class="fourthlabel">Amazing Excel</h3>
-<input type="submit" class="form-control"  name="mt" id="useName" value="Amazing excel sẽ giúp bạn tính toán và xuất file excel.">
-</div>
-</div>
-<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
-<div class="animated features-ml">
-<img src="https://img.icons8.com/fluent/72/26e07f/composing-mail.png"/>
-<h3 class="fourthlabel">Amazing mailbox</h3>
-<input type="submit" class="form-control"  name="mailbox" id="mailbox" value="Amazing mailbox sẽ giúp bạn gửi mail.">
-</div>
-</div>
 </div>
 </div>
 </form>
