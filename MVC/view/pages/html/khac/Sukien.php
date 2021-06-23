1a143c6d4cc02b330b381b6b72f9748de92c44ad
MrTom2020 <tomhumchin2@gmail.com>
2021-06-23 14:03:21 +0700

    make it better

--- MVC/view/pages/html/khac/Sukien.php
+++ MVC/view/pages/html/khac/Sukien.php
@@ -9,7 +9,7 @@
     <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact1" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
   </li>
 </ul>
-<div class="tab-content" id="myTabContent">
+<div class="tab-content" id="myTabContent1">
   <div class="tab-pane fade show active" id="home1" role="tabpanel" aria-labelledby="home-tab">1...</div>
   <div class="tab-pane fade" id="profile1" role="tabpanel" aria-labelledby="profile-tab">2...</div>
   <div class="tab-pane fade" id="contact1" role="tabpanel" aria-labelledby="contact-tab">3...</div>
