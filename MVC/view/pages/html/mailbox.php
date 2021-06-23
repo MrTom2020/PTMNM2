b73409382aa882d2066ff1393167d02fff36c796
MrTom2020 <tomhumchin2@gmail.com>
2021-06-23 09:53:26 +0700

    make it better

--- MVC/view/pages/html/mailbox.php
+++ MVC/view/pages/html/mailbox.php
@@ -1,7 +1,7 @@
 <nav>
         <?php include_once('menu_main/menu.php') ?>
    </nav>
-   <section>
+   <section style="width:100vw;">
      <?php include_once('khac/mailbox.php') ?>
    </section>
    
