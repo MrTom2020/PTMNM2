d9164413bf9d625796ca9378e79606848b31477d
MrTom2020 <tomhumchin2@gmail.com>
2021-06-23 16:33:17 +0700

    make it better

--- MVC/view/pages/html/tienich/giaodich.php
+++ MVC/view/pages/html/tienich/giaodich.php
@@ -1,4 +1,4 @@
-<form  action="../tienich/ti" method="POST">
+<form  action="../sogiaodich/ti" method="POST">
 <div class="max-width text-center middle-things" style="margin-top:15vh;">
 <div class="row">
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn" style="margin-bottom:5vh;">
@@ -12,7 +12,7 @@
 <div class="animated features-ml">
 <img src="https://img.icons8.com/fluent/71/26e07f/estimate.png"/>
 <h3 class="fourthlabel">Sổ Thu</h3>
-<input type="submit" class="form-control"  name="mt" id="useName" value="Một công cụ ghi chép tiền vào.">
+<input type="submit" class="form-control"  name="st" id="useName" value="Một công cụ ghi chép tiền vào.">
 </div>
 </div>
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
