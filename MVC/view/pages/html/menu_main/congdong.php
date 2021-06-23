06394123cc2d5e557d945ebcf73b304a5cdc7f97
MrTom2020 <tomhumchin2@gmail.com>
2021-06-23 17:15:47 +0700

    make it better

--- MVC/view/pages/html/menu_main/congdong.php
+++ MVC/view/pages/html/menu_main/congdong.php
@@ -4,21 +4,21 @@
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn" style="margin-bottom:5vh;">
 <div class="animated features-ml">
 <img src="https://img.icons8.com/fluent/71/26e07f/calculator.png"/>
-<h3 class="fourthlabel">Sổ Chi</h3>
+<h3 class="fourthlabel">Tin tức</h3>
 <input type="submit" class="form-control"  name="mt" id="useName" value="Một công cụ ghi chép tiền xuất.">
 </div>
 </div>
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
 <div class="animated features-ml">
 <img src="https://img.icons8.com/fluent/71/26e07f/estimate.png"/>
-<h3 class="fourthlabel">Sổ Thu</h3>
+<h3 class="fourthlabel">Sự kiện</h3>
 <input type="submit" class="form-control"  name="st" id="useName" value="Một công cụ ghi chép tiền vào.">
 </div>
 </div>
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
 <div class="animated features-ml">
 <img src="https://img.icons8.com/fluent/71/26e07f/rupee-exchange.png"/>
-<h3 class="fourthlabel">Sổ đầu tư</h3>
+<h3 class="fourthlabel">Mẹo vặt</h3>
 <input type="submit" class="form-control"  name="cd" id="cd" value="Một công cụ ghi chép tiền đầu tư.">
 </div>
 </div>
@@ -27,18 +27,10 @@
 <div class="col-md-4 col-lg-4  col-xs-6 animated fadeIn" style="margin-bottom:5vh;">
 <div class="animated features-ml">
 <img src="https://img.icons8.com/dotty/71/26e07f/planner.png"/>
-<h3 class="fourthlabel">Sổ kế hoạch</h3>
+<h3 class="fourthlabel">Bí quyết</h3>
 <input type="submit" class="form-control"  name="mt" id="useName" value="Một công cụ ghi chép kế hoạch.">
 </div>
 </div>
-<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
-<div class="animated features-ml">
-<img src="https://img.icons8.com/pastel-glyph/71/26e07f/note.png"/>
-<h3 class="fourthlabel">Sổ Nợ</h3>
-<input type="submit" class="form-control"  name="mt" id="useName" value="Một công cụ ghi chép tiền nợ">
-</div>
-</div>
-
 </div>
 </div>
 </form>
