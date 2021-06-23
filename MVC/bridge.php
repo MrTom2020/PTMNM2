b5583bce0d0074ce47539ddeecd2a7a766f94285
MrTom2020 <tomhumchin2@gmail.com>
2021-06-23 10:16:15 +0700

    make it better

--- MVC/bridge.php
+++ MVC/bridge.php
@@ -2,6 +2,7 @@
    // require_once "MVC/view/te.php";
   // require_once "MVC/controler/k.php";
   //require_once "MVC/view/login.php";
+   include_once "public/css.php";
    require_once "MVC/core/app.php";
    require_once "MVC/core/Controller.php";
   // require_once "MVC/core/main.php";
