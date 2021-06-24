52bea0e0f284ade7e8e31529be1571799276c342
MrTom2020 <tomhumchin2@gmail.com>
2021-06-25 02:19:54 +0700

    make it better

--- MVC/controler/u.php
+++ MVC/controler/u.php
@@ -7,7 +7,7 @@ public $servername = "us-cdbr-east-04.cleardb.com";
 */
     $servername = "us-cdbr-east-04.cleardb.com";
     $username = "b3b5c4ce68fd3f";
-    $password = "YES";
+    $password = "00a554";
     $dbname = "heroku_59161a489581119";
     $conn = mysqli_connect($servername,$username,$password,$dbname);
     if(!$conn){
