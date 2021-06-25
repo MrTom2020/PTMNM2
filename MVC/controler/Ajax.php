1a2a7e6edec485dcc34e85f93d3e1ad8d439ddc6
MrTom2020 <tomhumchin2@gmail.com>
2021-06-25 08:42:05 +0700

    make it better

--- MVC/controler/Ajax.php
+++ MVC/controler/Ajax.php
@@ -15,44 +15,44 @@
        public function checkuse()
        {
             $un =  isset($_POST["un"]) ? $_POST["un"]:0;
-           // echo $this->modelcon->checkus($un);
-            if (isset($_POST['btnLogin']))
-            {
-                $name = isset($_POST['useName']) ? $_POST['useName']:"";
-                $password = isset($_POST['usePassword']) ? $_POST['usePassword']:"";
-                if(!$name && !$password)
-                {
-                    $this->view("main",[
-                        "Page"=>"main"
-                    ]);
-                }
-                else
-                {
-                    $this->view("Admin",[
-                        "Page"=>"HomeAdmin",
-                        "Page1"=>"index"
-                    ]);
-                }
-            }
-            if (isset($_POST['signup']))
-            {
-                $this->view("Signup",[
-                    "Page"=>"formsigup"
-                ]);
-            }
-            if (isset($_POST['qll']))
-            {
-                $this->view("tintuc",[
-                    "Page"=>"tienich"
-                ]);
-            }
-            if(isset($_POST["admin"]))
-            {
-                $this->view("Admin",[
-                    "Page"=>"HomeAdmin",
-                    "Page1"=>"index"
-                ]);
-            }
+            echo $this->modelcon->checkus($un);
+            // if (isset($_POST['btnLogin']))
+            // {
+            //     $name = isset($_POST['useName']) ? $_POST['useName']:"";
+            //     $password = isset($_POST['usePassword']) ? $_POST['usePassword']:"";
+            //     if(!$name && !$password)
+            //     {
+            //         $this->view("main",[
+            //             "Page"=>"main"
+            //         ]);
+            //     }
+            //     else
+            //     {
+            //         $this->view("Admin",[
+            //             "Page"=>"HomeAdmin",
+            //             "Page1"=>"index"
+            //         ]);
+            //     }
+            // }
+            // if (isset($_POST['signup']))
+            // {
+            //     $this->view("Signup",[
+            //         "Page"=>"formsigup"
+            //     ]);
+            // }
+            // if (isset($_POST['qll']))
+            // {
+            //     $this->view("tintuc",[
+            //         "Page"=>"tienich"
+            //     ]);
+            // }
+            // if(isset($_POST["admin"]))
+            // {
+            //     $this->view("Admin",[
+            //         "Page"=>"HomeAdmin",
+            //         "Page1"=>"index"
+            //     ]);
+            // }
        }
     }
 ?>
