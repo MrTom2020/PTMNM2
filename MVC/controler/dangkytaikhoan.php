2cb7c103b16eb899c5d53153b807bfea79a81a51
MrTom2020 <tomhumchin2@gmail.com>
2021-06-26 15:10:54 +0700

    make it better

--- MVC/controler/dangkytaikhoan.php
+++ MVC/controler/dangkytaikhoan.php
@@ -25,8 +25,8 @@
             $sdt = isset($_POST["sdt"]) ? $_POST["sdt"]:"";
             $cauhoibimat = isset($_POST["cauhoibimat"]) ? $_POST["cauhoibimat"]:"";
             $cautraloi = isset($_POST["cautraloi"]) ? $_POST["cautraloi"]:"";
-            $kk  = date("Y/m/d",strtotime($ngaysinh));
-            $this->a->checkus($hoten, $mk,$kk,$dc,$email,$cauhoibimat,$sdt,$cautraloi);
+           // $kk  = date("Y/m/d",strtotime($ngaysinh));
+            $this->a->checkus($hoten, $ngaysinh,$kk,$dc,$email,$cauhoibimat,$sdt,$cautraloi);
             $this->view("main",[
                 "Page"=>"main"
             ]);
