af14d1eabd57c7d38ec3789a093c4ae94346e6ef
MrTom2020 <tomhumchin2@gmail.com>
2021-06-28 22:28:17 +0700

    make it better

--- MVC/view/pages/js/signup.js
+++ MVC/view/pages/js/signup.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#hoten").keyup(function()
+    $("#form6Example1").keyup(function()
     {
         var user = $(this).val();
         $.post("../dangkytaikhoan/kiemtra",{un:user},function(data){ $("#htkq1").html(data); });
