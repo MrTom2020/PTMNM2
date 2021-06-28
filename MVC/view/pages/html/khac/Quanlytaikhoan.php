4f48efdf2d67eeff1613f488d9a115d88791e910
MrTom2020 <tomhumchin2@gmail.com>
2021-06-29 01:26:41 +0700

    make it better

--- MVC/view/pages/html/khac/Quanlytaikhoan.php
+++ MVC/view/pages/html/khac/Quanlytaikhoan.php
@@ -1,24 +1,24 @@
 <form  action="../tienich/ti" method="POST">
 <div class="max-width text-center middle-things">
-<h3 class="text-center text-up green visible-xs-block visible-sm-block"><?php echo isset($_SESSION['ten']) ?  $_SESSION['ten']:"" ?></h3>
+<h3 class="text-center text-up green visible-xs-block visible-sm-block">Hi !<?php echo isset($_SESSION['ten']) ?  $_SESSION['ten']:"" ?></h3>
 <div class="row">
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn" style="margin-bottom:5vh;">
 <div class="animated features-ml">
-<img src="https://img.icons8.com/fluent/71/26e07f/calculator.png"/>
+<img src="https://img.icons8.com/ios-filled/50/26e07f/wallet.png"/>
 <h3 class="fourthlabel">Amazing wallet</h3>
 <input type="submit" class="form-control"  name="mt" id="useName" value="Amazing calculator là một công cụ tính toán tuyệt vời.">
 </div>
 </div>
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
 <div class="animated features-ml">
-<img src="https://img.icons8.com/fluent/71/26e07f/estimate.png"/>
+<img src="https://img.icons8.com/ios-filled/50/26e07f/wallet.png"/>
 <h3 class="fourthlabel">Amazing list wallet</h3>
 <input type="submit" class="form-control"  name="mt" id="useName" value="Amazing split the money là công cụ chia tiền hữu ích tiền.">
 </div>
 </div>
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
 <div class="animated features-ml">
-<img src="https://img.icons8.com/fluent/71/26e07f/rupee-exchange.png"/>
+<img src="https://img.icons8.com/ios-filled/50/26e07f/wallet.png"/>
 <h3 class="fourthlabel">Amazing member wallet</h3>
 <input type="submit" class="form-control"  name="cd" id="cd" value="Amazing money conversion là công cụ chuyển đổi dv tiền.">
 </div>
@@ -27,21 +27,21 @@
 
 <div class="col-md-4 col-lg-4  col-xs-6 animated fadeIn" style="margin-bottom:5vh;">
 <div class="animated features-ml">
-<img src="https://img.icons8.com/dotty/71/26e07f/planner.png"/>
+<img src="https://img.icons8.com/ios-filled/50/26e07f/wallet.png"/>
 <h3 class="fourthlabel">Amazing update wallet</h3>
 <input type="submit" class="form-control"  name="mt" id="useName" value="Amazing plan giúp bạn lập kế hoạch dễ dàng.">
 </div>
 </div>
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
 <div class="animated features-ml">
-<img src="https://img.icons8.com/pastel-glyph/71/26e07f/note.png"/>
+<img src="https://img.icons8.com/ios-filled/50/26e07f/group-background-selected.png"/>
 <h3 class="fourthlabel">Amazing personal information</h3>
 <input type="submit" class="form-control"  name="mt" id="useName" value="Amazing loan calculation sẽ giúp bạn ghi nhớ các khoản vay.">
 </div>
 </div>
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
 <div class="animated features-ml">
-<img src="https://img.icons8.com/fluent/71/26e07f/growing-money.png"/>
+<img src="https://img.icons8.com/ios-filled/50/26e07f/group-background-selected.png"/>
 <h3 class="fourthlabel">Amazing delete members</h3>
 <input type="submit" class="form-control"  name="mt" id="useName" value="Amazing loan calculation sẽ giúp bạn tính các khoản.">
 </div>
