9b1b8f05c8f4bd5c411b9704593504379bbf1736
MrTom2020 <tomhumchin2@gmail.com>
2021-06-29 00:04:56 +0700

    make it better

--- MVC/models/modeldktk.php
+++ MVC/models/modeldktk.php
@@ -19,7 +19,7 @@
             if($r)
             {
                 $row = mysqli_fetch_array($r);
-                $kq = $row;
+                $kq = $row[0];
                 // if($row != 1)
                 // {
                 //     $kq = "Bạn có thể dùng thông tin đó";
