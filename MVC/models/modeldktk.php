506e5e9a634a7efe9b487173cf5d3bfef5782a26
MrTom2020 <tomhumchin2@gmail.com>
2021-06-28 20:41:47 +0700

    make it better

--- MVC/models/modeldktk.php
+++ MVC/models/modeldktk.php
@@ -1,7 +1,7 @@
 <?php
     class modeldktk extends DB
     {
-        public function checkus($username,$password,$ngaysinh,$diachi,$email,$cauhoibimat,$sdt,$cautraloi,$ngaythamgia)
+        public function checkus($username,$password,$ngaysinh,$diachi,$email,$cauhoibimat,$sdt,$cautraloi,$ngaytg)
         {
             $kq = "Đăng ký tài khoản không thành công";
             $sql = "CALL themtaikhoan('$username','$password','$ngaysinh','$diachi','$email','$cauhoibimat','$cautraloi','$sdt','$ngaytg')";
