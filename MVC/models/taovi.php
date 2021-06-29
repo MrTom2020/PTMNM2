9504aea42e9e54703b287b40c4e091553259ce4d
MrTom2020 <tomhumchin2@gmail.com>
2021-06-29 11:10:58 +0700

    make it better

--- MVC/models/taovi.php
+++ MVC/models/taovi.php
@@ -5,7 +5,7 @@
     {
         $ten1 = isset($_SESSION['ten']) ?  $_SESSION['ten']:"2";
        // $kq = 1;
-        $sql2 = "CALL danhsachnguoidung('$ten')";
+        $sql2 = "CALL danhsachnguoidung('$ten1')";
         $resut1 = mysqli_query($conn,$sql2);
         $k = mysqli_fetch_array($resut1);
         //$sql = "CALL taovi('$tenvi','$tien','$k','$ngaytao','$loai')";
