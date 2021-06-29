122455328a520b23145570dfa116a7b8b9eaf0c6
MrTom2020 <tomhumchin2@gmail.com>
2021-06-29 11:45:49 +0700

    make it better

--- MVC/controler/quanlytaikhoan.php
+++ MVC/controler/quanlytaikhoan.php
@@ -32,9 +32,11 @@
     {
         if(isset($_POST["tv"]))
         {
-            $this->view("tienich",[
-                "Page"=>"tienich"
-            ]);
+                $tenvi = isset($_POST["tenvi"]) ? $_POST["tenvi"]:"";
+            $tien = isset($_POST["tienvi"]) ? $_POST["tienvi"]:"";
+            $ngaytao = isset($_POST["ngaytao"]) ? $_POST["ngaytao"]:"";
+            $loaivi = isset($_POST["loaivi"]) ? $_POST["loaivi"]:"";
+         echo $this->a->taovi1($tenvi,$tien,$ngaytao,$loaivi); 
         }
         //     $tenvi = isset($_POST["tenvi"]) ? $_POST["tenvi"]:"";
         //     $tien = isset($_POST["tienvi"]) ? $_POST["tienvi"]:"";
