60f20810b6288cebb879468d9be27448c59fe834
MrTom2020 <tomhumchin2@gmail.com>
2021-06-29 11:06:37 +0700

    make it better

--- MVC/models/taovi.php
+++ MVC/models/taovi.php
@@ -5,16 +5,16 @@
     {
         $ten1 = isset($_SESSION['ten']) ?  $_SESSION['ten']:"2";
        // $kq = 1;
-        //$sql2 = "CALL danhsachnguoidung('$ten')";
-        //$resut1 = mysqli_query($conn,$sql2);
-        //$k = mysqli_fetch_array($resut1);
+        $sql2 = "CALL danhsachnguoidung('$ten')";
+        $resut1 = mysqli_query($conn,$sql2);
+        $k = mysqli_fetch_array($resut1);
         //$sql = "CALL taovi('$tenvi','$tien','$k','$ngaytao','$loai')";
        // $rowss = mysqli_query($this->con,$sql);
         //if(mysqli_query($this->con,$sql))
         {
             $kq = $ten1;
         }
-            return $kq;
+            return $k[0];
     }
     }
 ?>
