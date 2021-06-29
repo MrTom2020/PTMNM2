842973aeb75eff7c189691b66394e512eb44ce7d
MrTom2020 <tomhumchin2@gmail.com>
2021-06-29 18:35:51 +0700

    make it better

--- MVC/models/taovi.php
+++ MVC/models/taovi.php
@@ -3,9 +3,10 @@
     {
     public function taovi1($tenvi,$tien,$ngaytao,$loai)
     {
+       $kk = isset($_SESSION['iduu']) ? $_SESSION['iduu']:"";
         $kq = 0;
        $ten1 = isset($_SESSION['ten']) ?  $_SESSION['ten']:"2";
-       $sql = "CALL taovi('$tenvi','$tien','$ten1','$ngaytao','$loai')";
+       $sql = "CALL taovi('$tenvi','$tien','$kk','$ngaytao','$loai')";
        mysqli_query($this->con,$sql);
        return $ten1;
     }
