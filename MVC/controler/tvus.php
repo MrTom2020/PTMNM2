944ccc0439b6e81e09e90981475498daa3967863
MrTom2020 <tomhumchin2@gmail.com>
2021-06-29 16:57:13 +0700

    make it better

--- MVC/controler/tvus.php
+++ MVC/controler/tvus.php
@@ -20,15 +20,7 @@
             $tien = isset($_POST["tienvi"]) ? $_POST["tienvi"]:"";
             $ngaytao = isset($_POST["ngaytao"]) ? $_POST["ngaytao"]:"";
             $loaivi = isset($_POST["loaivi"]) ? $_POST["loaivi"]:"";
-         $k =  $this->a->taovi1($tenvi,$tien,$ngaytao,$loaivi);
-         if($k == 1)
-         {
-            echo '32';
-         }
-         else
-         {
-             echo '3';
-         }
+         echo  $this->a->taovi1($tenvi,$tien,$ngaytao,$loaivi);
         }
     }
 }
