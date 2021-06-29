243342bec7a9a62eba39885ecb3e323efccd850a
MrTom2020 <tomhumchin2@gmail.com>
2021-06-29 11:54:27 +0700

    make it better

--- MVC/controler/quanlytaikhoan.php
+++ MVC/controler/quanlytaikhoan.php
@@ -26,7 +26,7 @@
             $tien = isset($_POST["tienvi"]) ? $_POST["tienvi"]:"";
             $ngaytao = isset($_POST["ngaytao"]) ? $_POST["ngaytao"]:"";
             $loaivi = isset($_POST["loaivi"]) ? $_POST["loaivi"]:"";
-         echo $this->a->taovi1($tenvi,$tien,$ngaytao,$loaivi);
+         echo $loaivi;// $this->a->taovi1($tenvi,$tien,$ngaytao,$loaivi);
         }
     }
 }
