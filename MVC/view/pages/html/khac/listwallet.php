711afe94bb43604feeab71423fb220ac550eb0f1
MrTom2020 <tomhumchin2@gmail.com>
2021-06-29 19:55:19 +0700

    make it better

--- MVC/view/pages/html/khac/listwallet.php
+++ MVC/view/pages/html/khac/listwallet.php
@@ -1,3 +1,8 @@
 <?php
-    echo '123';
+    echo "<select class='form-select' aria-label='Default select example'>";
+    for($i = 0;$i < 10;++$i)
+    {
+        echo "<option value='1'>One</option>";
+    }
+    echo "</select>";
  ?>
