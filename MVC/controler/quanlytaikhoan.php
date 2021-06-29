7ad3790a6bc4f33ac44893b7417658455ae3a39d
MrTom2020 <tomhumchin2@gmail.com>
2021-06-29 10:31:55 +0700

    make it better

--- MVC/controler/quanlytaikhoan.php
+++ MVC/controler/quanlytaikhoan.php
@@ -30,7 +30,7 @@
             $userid = isset($_POST["tenvi"]) ? $_POST["tenvi"]:"";
             $ngaytao = isset($_POST["ngaytao"]) ? $_POST["ngaytao"]:"";
             $loaivi = isset($_POST["loaivi"]) ? $_POST["loaivi"]:"";
-            $this->a->taovi($tenvi, $tien,$userid,$ngaytao,$loaivi);
+            $this->a->taovi($tenvi, $tien,$ngaytao,$loaivi);
             $this->view("main",[
                 "Page"=>"main"
             ]);
--- MVC/models/taovi.php
+++ MVC/models/taovi.php
@@ -1,16 +1,18 @@
 <?php
     class taovi extends DB
     {
-    public function taovi($tenvi,$tien,$userid,$ngaytao,$loai)
+    public function taovi($tenvi,$tien,$ngaytao,$loai)
     {
+        $ten = isset($_SESSION['ten']) ?  $_SESSION['ten']:"";
         $kq = 1;
-        $sql = "CALL taovi('$tenvi','$tien','$userid','$ngaytao','$loai')";
-            $rowss = mysqli_query($this->con,$sql);
-
-            if(mysqli_num_rows($rowss) > 0)
-            {
-                $kq = 0;
-            }
+        $sql2 = "CALL danhsachnguoidung('$ten')";
+        $k = mysqli_fetch_array($sql2);
+        $sql = "CALL taovi('$tenvi','$tien','$k','$ngaytao','$loai')";
+       // $rowss = mysqli_query($this->con,$sql);
+        if(mysqli_query($this->con,$sql))
+        {
+            $kq = 0;
+        }
             return $kq;
     }
     }
