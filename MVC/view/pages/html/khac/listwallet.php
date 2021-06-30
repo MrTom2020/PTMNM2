99703fb511f7245e9fda23f560efedbc3a0ec64d
MrTom2020 <tomhumchin2@gmail.com>
2021-06-30 23:07:33 +0700

    make it better

--- MVC/view/pages/html/khac/listwallet.php
+++ MVC/view/pages/html/khac/listwallet.php
@@ -7,6 +7,7 @@
       <th scope='col'>First</th>
       <th scope='col'>Last</th>
       <th scope='col'>Handle</th>
+      <th scope='col'>Handle</th>
       <th scope='col'>Xóa</th>
     </tr>
      </thead>";
@@ -14,10 +15,11 @@
      while($row = mysqli_fetch_array($dt))
     {
         echo "<tr>
-        <th scope='row'>1</th>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
+        <th scope='row'>$row[0]</th>
+        <td>$row[1]</td>
+        <td>$row[2]</td>
+        <td>$row[3]</td>
+        <td>$row[4]</td>
         <td><img src='https://img.icons8.com/ios/50/000000/delete--v3.png'/></td>
         </tr>";
       echo $row[1];
