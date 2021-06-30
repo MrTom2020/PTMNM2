76e49a2e17cd5c0fb3808f5436d97bd4bf419ae7
MrTom2020 <tomhumchin2@gmail.com>
2021-06-30 23:43:46 +0700

    make it better

--- MVC/view/pages/html/khac/listwallet.php
+++ MVC/view/pages/html/khac/listwallet.php
@@ -24,31 +24,7 @@
         <td>$k</td>
         <td><img src='https://img.icons8.com/ios/50/000000/delete--v3.png'/></td>
         </tr>";
-      echo $row[1];
     }
     echo "</tbody>";
     echo "</table>";
-    // echo "<table class='table'>";
-    // echo "<thead>
-    // <tr>
-    //   <th scope='col'>#</th>
-    //   <th scope='col'>First</th>
-    //   <th scope='col'>Last</th>
-    //   <th scope='col'>Handle</th>
-    //   <th scope='col'>Xóa</th>
-    // </tr>
-    //  </thead>";
-    //  echo "<tbody>";
-    // for($i = 0;$i < 10;++$i)
-    // {
-    //     echo " <tr>
-    //     <th scope='row'>$i</th>
-    //     <td>$i</td>
-    //     <td>$i</td>
-    //     <td>$i</td>
-    //     <td><img src='https://img.icons8.com/ios/50/000000/delete--v3.png'/></td>
-    //     </tr>";
-    // }
-    // echo "</tbody>";
-    // echo "</table>";
  ?>
