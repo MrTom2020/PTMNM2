8cfb1f687c5ee2f7da5d7522de5dc512a4a0855d
MrTom2020 <tomhumchin2@gmail.com>
2021-07-01 22:31:44 +0700

    make it better

--- MVC/models/thuvachitieu.php
+++ MVC/models/thuvachitieu.php
@@ -5,12 +5,12 @@
         public function taochitieu($ten,$loai,$idvi,$ngaytao,$giatri,$danhmuc)
         {
             $kq = "";
-            // $idd = isset($_SESSION['iduu']) ? $_SESSION['iduu']:"";
-            // $sql = "CALL taokhoanthuchi('$ten','$loai','$idvi','$ngaytao','$giatri','$danhmuc')";
-            // if(mysqli_query($this->con,$sql))
-            // {
-            //     $kq = "Đăng ký tài khoản thành công";
-            // }
+            $idd = isset($_SESSION['iduu']) ? $_SESSION['iduu']:"";
+            $sql = "CALL taokhoanthuchi('$ten','$loai','$idvi','$ngaytao','$giatri','$danhmuc')";
+            if(mysqli_query($this->con,$sql))
+            {
+                $kq = "Tạo khoản chi thành công";
+            }
             return $kq;
         }
     }
