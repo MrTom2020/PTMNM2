b26e61d050bce4dd9b15b84a73d9d978ffab85fd
MrTom2020 <tomhumchin2@gmail.com>
2021-07-03 01:55:54 +0700

    make it better

--- MVC/controler/Quanlynguoidung.php
+++ MVC/controler/Quanlynguoidung.php
@@ -18,7 +18,11 @@
         if(isset($_POST["kp"]))
         {
             $mand = isset($_POST['kp']) ? $_POST['kp']:"0";
-            echo $this->a->kpuss($mand);
+            $this->a->kpuss($mand);
+            $this->view("Admin",[
+                "Page"=>"HomeAdmin",
+                "Page1"=>"index3"
+            ]);
         }
     }
     public function vohieund()
@@ -26,7 +30,11 @@
         if(isset($_POST["vh"]))
         {
             $mand = isset($_POST['vh']) ? $_POST['vh']:"0";
-            echo $this->a->vhuss($mand);
+            $this->a->vhuss($mand);
+            $this->view("Admin",[
+                "Page"=>"HomeAdmin",
+                "Page1"=>"index2"
+            ]);
         }
     }
 }
