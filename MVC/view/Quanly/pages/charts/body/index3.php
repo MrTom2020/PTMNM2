dd6ceabe3fec8215f8d2497a72f1c213a5b74def
MrTom2020 <tomhumchin2@gmail.com>
2021-07-03 00:10:24 +0700

    make it better

--- MVC/view/Quanly/pages/charts/body/index3.php
+++ MVC/view/Quanly/pages/charts/body/index3.php
@@ -119,7 +119,7 @@
         <td>$row[9]</td>
         <td>$row[10]</td>
         <td>$row[11]</td>
-        <td><input type='submit' name='kp'/></td>
+        <td> <button type='submit' class='btn btn-primary'>Submit</button></td>
         </tr>";
     }
     echo "</tbody>";
