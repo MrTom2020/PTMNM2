b5f03c27d1cb94f71da3bc8b45bb49b567f4c7dd
MrTom2020 <tomhumchin2@gmail.com>
2021-07-03 12:16:24 +0700

    make it better

--- MVC/view/Quanly/pages/charts/body/index.php
+++ MVC/view/Quanly/pages/charts/body/index.php
@@ -30,7 +30,7 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__wobble" src="MVC/view/Quanly/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__wobble" src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
   </div>
 
   <!-- Navbar -->
@@ -41,7 +41,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="MVC/view/Quanly/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Admin</span>
     </a>
 
@@ -50,7 +50,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="MVC/view/Quanly/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block"><?php echo isset($_SESSION['ten']) ? $_SESSION['ten']:"";?></a>
@@ -161,38 +161,38 @@
   </footer>
 </div>
 <!-- ./wrapper -->
-<script src="MVC/view/Quanly/plugins/jquery/jquery.min.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/plugins/jquery/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="MVC/view/Quanly/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="MVC/view/Quanly/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- ChartJS -->
-<script src="MVC/view/Quanly/plugins/chart.js/Chart.min.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/plugins/chart.js/Chart.min.js"></script>
 <!-- Sparkline -->
-<script src="MVC/view/Quanly/plugins/sparklines/sparkline.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/plugins/sparklines/sparkline.js"></script>
 <!-- JQVMap -->
-<script src="MVC/view/Quanly/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="MVC/view/Quanly/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="MVC/view/Quanly/plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/plugins/jquery-knob/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
-<script src="MVC/view/Quanly/plugins/moment/moment.min.js"></script>
-<script src="MVC/view/Quanly/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/plugins/moment/moment.min.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/plugins/daterangepicker/daterangepicker.js"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="MVC/view/Quanly/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
 <!-- Summernote -->
-<script src="MVC/view/Quanly/plugins/summernote/summernote-bs4.min.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/plugins/summernote/summernote-bs4.min.js"></script>
 <!-- overlayScrollbars -->
-<script src="MVC/view/Quanly/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
-<script src="MVC/view/Quanly/dist/js/adminlte.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/dist/js/adminlte.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="MVC/view/Quanly/dist/js/demo.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/dist/js/demo.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="MVC/view/Quanly/dist/js/pages/dashboard.js"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/Quanly/dist/js/pages/dashboard.js"></script>
 </body>
 </html>
