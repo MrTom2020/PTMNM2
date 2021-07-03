88d8404b6d987f2769c4aab08e3481f1d0f0d3d8
MrTom2020 <tomhumchin2@gmail.com>
2021-07-03 23:16:52 +0700

    make it better

--- MVC/models/modelcon.php
+++ MVC/models/modelcon.php
@@ -48,8 +48,11 @@
                 $_SESSION['iduu'] = $r[0];
                 $kk = isset($_SESSION['iduu']) ? $_SESSION['iduu']:"";
                 $sql1 = "SELECT * FROM thongbao where Userid='$kk' and loai ='1' and tinhtrang = '1' ";
+                $sql2 = "SELECT * FROM thongbao where loai ='2' ";
                 $row1 = mysqli_query($this->con,$sql1);
+                $row2 = mysqli_query($this->con,$sql2);
                 $_SESSION['tbcnn'] = $row1;
+                $_SESSION['tbchung'] = $row2;
                 $kq = $r[11];
             }
             return $kq;
---
+++ MVC/view/pages/html/thongbao/thongbaochung.php
@@ -0,0 +1,15 @@
+<?php
+    $dt = isset($_SESSION['tbchung']) ? $_SESSION['tbchung']:NULL;
+    
+     while($row = mysqli_fetch_array($dt))
+    {
+        echo "<div class='card' style='width: 27rem;'>
+        <div class='card-body'>
+          <h5 class='card-title'>$row[3]</h5>
+          <p class='card-text'></p>$row[4]</p>
+          <p>Ngày đăng  <br/> $row[5]</p>
+        </div>
+      </div>";
+
+    }
+ ?>
