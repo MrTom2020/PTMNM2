3ba1a9b624670e4e14c211499e788da2f45c1231
MrTom2020 <tomhumchin2@gmail.com>
2021-07-04 00:14:33 +0700

    make it better

--- MVC/view/pages/html/thongbao/thongbaochung.php
+++ MVC/view/pages/html/thongbao/thongbaochung.php
@@ -6,8 +6,8 @@
         echo "<div class='card' style='width: 27rem;'>
         <div class='card-body'>
           <h5 class='card-title'>$row[3]</h5>
-          <p class='card-text' style='margin-bottom:15vh;'></p>$row[4]</p>
-          <p>Ngày đăng  <br/> $row[5]</p>
+          <p class='card-text'></p>$row[4]</p>
+          <p  style='margin-top:15vh;'>Ngày đăng  <br/> $row[5]</p>
         </div>
       </div>";
 
--- MVC/view/pages/html/thongbao/thongbaorieng.php
+++ MVC/view/pages/html/thongbao/thongbaorieng.php
@@ -7,8 +7,8 @@
         <div class='card' style='width: 27rem;'>
         <div class='card-body'>
           <h5 class='card-title'>$row[3]</h5>
-          <p class='card-text' style='margin-bottom:15vh;'></p>$row[4]</p>
-          <p>Ngày đăng  <br/> $row[5]</p>
+          <p class='card-text'></p>$row[4]</p>
+          <p style='margin-top:15vh;'>Ngày đăng  <br/> $row[5]</p>
         </div>
       </div>
       <button type='submit' name='xoatb' value='$row[0]' class='btn btn-primary'>Xóa</button>
