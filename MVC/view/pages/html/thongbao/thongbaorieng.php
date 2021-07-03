eaa6ad98e0db2b02148650fc280d43b49639585f
MrTom2020 <tomhumchin2@gmail.com>
2021-07-04 01:59:55 +0700

    make it better

--- MVC/view/pages/html/thongbao/thongbaorieng.php
+++ MVC/view/pages/html/thongbao/thongbaorieng.php
@@ -1,19 +1,30 @@
 <?php
   echo '<h1>THÔNG BÁO</h1>';
     $dt = isset($data['tbc']) ? $data['tbc']:NULL;
-    
+    echo "<table class='table'>
+    <thead>
+      <tr>
+        <th scope='col'>Tiêu đề</th>
+        <th scope='col'>Nội dung</th>
+        <th scope='col'></th>
+        <th scope='col'></th>
+        <th scope='col'>Ngày đăng</th>
+        <th scope='col'>Xóa</th>
+      </tr>
+    </thead>
+    <tbody>";
      while($row = mysqli_fetch_array($dt))
     {
-        echo "<form class='login-container' action='../xemthongbao/xemtb' method='POST'>
-        <div class='card' style='width: 27rem;'>
-        <div class='card-body'>
-          <h5 class='card-title'>$row[3]</h5>
-          <p class='card-text'></p>$row[4]</p>
-          <p style='margin-top:15vh;'>Ngày đăng  <br/> $row[5]</p>
-        </div>
-      </div>
-      <button type='submit' name='xoatb' value='$row[0]' class='btn btn-primary'>Xóa</button>
-      </form>";
 
+        echo "<form class='login-container' action='../xemthongbao/xemtb' method='POST'>
+        <tr>
+      <td>$row[3]</td>
+      <td colspan='3'>$row[4]</td>
+      <td>$row[5]</td>
+      <td><button type='submit' name='xoatb' value='$row[0]' class='btn btn-primary'>Xóa</button></td>
+       </tr> 
+      </form>";
     }
+    echo "</tbody>
+    </table>";
  ?>
