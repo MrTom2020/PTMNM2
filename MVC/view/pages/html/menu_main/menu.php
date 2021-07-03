f83e83ddf98f36b586acc2301d60ab82ac0c9954
MrTom2020 <tomhumchin2@gmail.com>
2021-07-04 00:35:28 +0700

    make it better

--- MVC/view/pages/html/menu_main/menu.php
+++ MVC/view/pages/html/menu_main/menu.php
@@ -15,10 +15,10 @@
 </nav>
 <div class="tab-content" id="nav-tabContent">
   <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-    <?php include_once('MVC/view/pages/html/thongbao/thongbaochung.php'); ?>
+    <!-- <?php //include_once('MVC/view/pages/html/thongbao/thongbaochung.php'); ?> -->
   </div>
   <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-  <?php include_once('MVC/view/pages/html/thongbao/thongbaorieng.php'); ?>
+  <!-- <?php //include_once('MVC/view/pages/html/thongbao/thongbaorieng.php'); ?> -->
   </div>
   <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
   789
