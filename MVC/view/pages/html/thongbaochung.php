ecfaf3cc718cad4002f34e0b00e993043a348183
MrTom2020 <tomhumchin2@gmail.com>
2021-07-04 01:22:32 +0700

    make it better

--- MVC/view/pages/html/thongbaochung.php
+++ MVC/view/pages/html/thongbaochung.php
@@ -1,30 +1,7 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
 <nav>
         <?php include_once('menu_main/menu.php') ?>
    </nav>
-   123333
-  <?php
-    $dt = isset($_SESSION['tbchung']) ? $_SESSION['tbchung']:NULL;
-     while($row = mysqli_fetch_array($dt))
-    {
-        echo "<div class='card' style='width: 27rem;'>
-        <div class='card-body'>
-          <h5 class='card-title'>$row[3]</h5>
-          <p class='card-text'></p>$row[4]</p>
-          <p  style='margin-top:15vh;'>Ngày đăng  <br/> $row[5]</p>
-        </div>
-      </div>";
-    }
-    echo '123';
- ?>
-</body>
-</html>
+   <section>
+     <?php include_once('khac/themtaikhoan.php') ?>
+   </section>
