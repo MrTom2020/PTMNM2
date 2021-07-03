122dbb231b787aaed53a778bc706f10884961b67
MrTom2020 <tomhumchin2@gmail.com>
2021-07-03 21:40:10 +0700

    make it better

---
+++ MVC/controler/xemthongbao.php
@@ -0,0 +1,24 @@
+<?php
+    class xemthongbao extends Controller
+    {
+        public $a;
+        public function __construct()
+        {
+            $this->a = $this->model("modelcon");
+        }
+    public function SayHi()
+    {
+        $this->view("login",[
+            "Page"=>"login",
+        ]);
+    }
+    public function xemtb()
+    {
+        if(isset($_POST["xoatb"]))
+        {
+            echo '123';
+        }
+    }
+}
+    
+?>
