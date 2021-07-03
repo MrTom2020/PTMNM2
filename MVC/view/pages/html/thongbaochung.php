4ae68211dee042d414052611fed2376edae508c3
MrTom2020 <tomhumchin2@gmail.com>
2021-07-04 01:07:09 +0700

    make it better

--- MVC/view/pages/html/thongbaochung.php
+++ MVC/view/pages/html/thongbaochung.php
@@ -5,7 +5,6 @@
 <div class="tab-content" id="myTabContent">
   <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab"><?php
     $dt = isset($_SESSION['tbchung']) ? $_SESSION['tbchung']:NULL;
-    
      while($row = mysqli_fetch_array($dt))
     {
         echo "<div class='card' style='width: 27rem;'>
@@ -15,6 +14,6 @@
           <p  style='margin-top:15vh;'>Ngày đăng  <br/> $row[5]</p>
         </div>
       </div>";
-
+      echo '123';
     }
  ?></div>
