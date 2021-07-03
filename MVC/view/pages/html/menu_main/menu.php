2de2dda7d2c141d8549268c8fd46be0a104aa6d6
MrTom2020 <tomhumchin2@gmail.com>
2021-07-04 00:43:17 +0700

    make it better

--- MVC/view/pages/html/menu_main/menu.php
+++ MVC/view/pages/html/menu_main/menu.php
@@ -15,41 +15,10 @@
 </nav>
 <div class="tab-content" id="nav-tabContent">
   <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-  <?php
-    $dt = isset($_SESSION['tbchung']) ? $_SESSION['tbchung']:NULL;
-    
-     while($row = mysqli_fetch_array($dt))
-    {
-        echo "<div class='card' style='width: 27rem;'>
-        <div class='card-body'>
-          <h5 class='card-title'>$row[3]</h5>
-          <p class='card-text'></p>$row[4]</p>
-          <p  style='margin-top:15vh;'>Ngày đăng  <br/> $row[5]</p>
-        </div>
-      </div>";
-
-    }
- ?>
+    <?php include_once('MVC/view/pages/html/thongbao/thongbaochung.php'); ?>
   </div>
   <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-  <?php
-    $dt = isset($_SESSION['tbcnn']) ? $_SESSION['tbcnn']:NULL;
-    
-     while($row = mysqli_fetch_array($dt))
-    {
-        echo "<form class='login-container' action='../xemthongbao/xemtb' method='POST'>
-        <div class='card' style='width: 27rem;'>
-        <div class='card-body'>
-          <h5 class='card-title'>$row[3]</h5>
-          <p class='card-text'></p>$row[4]</p>
-          <p style='margin-top:15vh;'>Ngày đăng  <br/> $row[5]</p>
-        </div>
-      </div>
-      <button type='submit' name='xoatb' value='$row[0]' class='btn btn-primary'>Xóa</button>
-      </form>";
-
-    }
- ?>
+  <!-- <?php //include_once('MVC/view/pages/html/thongbao/thongbaorieng.php'); ?> -->
   </div>
   <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
   789
---
+++ MVC/view/pages/html/thongbao/thongbaorieng.php
@@ -0,0 +1,18 @@
+<?php
+    $dt = isset($_SESSION['tbcnn']) ? $_SESSION['tbcnn']:NULL;
+    
+     while($row = mysqli_fetch_array($dt))
+    {
+        echo "<form class='login-container' action='../xemthongbao/xemtb' method='POST'>
+        <div class='card' style='width: 27rem;'>
+        <div class='card-body'>
+          <h5 class='card-title'>$row[3]</h5>
+          <p class='card-text'></p>$row[4]</p>
+          <p style='margin-top:15vh;'>Ngày đăng  <br/> $row[5]</p>
+        </div>
+      </div>
+      <button type='submit' name='xoatb' value='$row[0]' class='btn btn-primary'>Xóa</button>
+      </form>";
+
+    }
+ ?>
