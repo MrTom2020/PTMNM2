6f721aa9470111791582e567e01100beea1ce294
MrTom2020 <tomhumchin2@gmail.com>
2021-07-04 01:19:56 +0700

    make it better

--- MVC/view/pages/html/thongbaochung.php
+++ MVC/view/pages/html/thongbaochung.php
@@ -1,4 +1,13 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
 <nav>
         <?php include_once('menu_main/menu.php') ?>
    </nav>
@@ -17,3 +26,5 @@
     }
     echo '123';
  ?>
+</body>
+</html>
