831839b1d69bb2cb9018566557c900442f6d8f9d
MrTom2020 <tomhumchin2@gmail.com>
2021-07-03 21:48:57 +0700

    make it better

--- MVC/view/pages/html/thongbao/thongbaorieng.php
+++ MVC/view/pages/html/thongbao/thongbaorieng.php
@@ -6,12 +6,12 @@
         echo "<form class='login-container' action='../xemthongbao/xemtb' method='POST'>
         <div class='card' style='width: 27rem;'>
         <div class='card-body'>
-          <h5 class='card-title'>$row[3]</h5>
+          <h5 class='card-title' name='tieudend'>$row[3]</h5>
           <p class='card-text'>$row[4]</p>
           <p>Ngày đăng  <br/> $row[5]</p>
         </div>
       </div>
-      <input class='dropdown-item' name='xoatb' type='submit' value='$row[0]'>Xóa</input>
+      <input class='dropdown-item' name='xoatb' type='submit' value='Xóa'>
       </form>";
 
     }
