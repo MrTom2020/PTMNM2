323a297cd133e24b8518e76278d95916921096b7
MrTom2020 <tomhumchin2@gmail.com>
2021-07-04 00:44:48 +0700

    make it better

--- MVC/view/pages/html/menu_main/menu.php
+++ MVC/view/pages/html/menu_main/menu.php
@@ -15,7 +15,7 @@
 </nav>
 <div class="tab-content" id="nav-tabContent">
   <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-    <?php include_once('MVC/view/pages/html/thongbao/thongbaochung.php'); ?>
+    <?php require_once('MVC/view/pages/html/thongbao/thongbaochung.php'); ?>
   </div>
   <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
   <!-- <?php //include_once('MVC/view/pages/html/thongbao/thongbaorieng.php'); ?> -->
