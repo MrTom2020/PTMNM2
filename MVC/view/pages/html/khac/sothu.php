46e48c8d9fd18422ea6359a941ead55e1d00c0cd
MrTom2020 <tomhumchin2@gmail.com>
2021-07-05 08:44:01 +0700

    make it better

--- MVC/view/pages/html/khac/sothu.php
+++ MVC/view/pages/html/khac/sothu.php
@@ -1,6 +1,17 @@
 <div style="width:70vw;margin-top:10vh;margin-left:15vw;">
   <form action="./../nhapkhoanthuchi/themkhoanthuchi" method="POST">
    <h1 class="text-center">Form Nhập khoản thu</h1>
+   <div class="form-outline mb-4">
+   <?php 
+   echo "<select name='tenvi' class='form-select' aria-label='Default select example'>";
+   while($row = mysqli_fetch_array($dt))
+   {
+     echo "<option value='$row[0]'>$row[1]</option>";
+   }
+  echo "</select>";
+    ?>
+    <label class="form-label" for="form6Example6">Ví</label>
+  </div>
   <div class="row mb-4">
     <div class="col">
       <div class="form-outline">
