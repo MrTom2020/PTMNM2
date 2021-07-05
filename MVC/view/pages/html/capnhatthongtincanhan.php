006df523f0ecacb506980f25d723dab42f409b93
MrTom2020 <tomhumchin2@gmail.com>
2021-07-05 14:27:42 +0700

    make it better

--- MVC/view/pages/html/capnhatthongtincanhan.php
+++ MVC/view/pages/html/capnhatthongtincanhan.php
@@ -1,6 +1,6 @@
 <nav>
         <?php include_once('menu_main/menu.php') ?>
    </nav>
-   <section style="height:85vh;">
+   <section style="height:100vh;">
      <?php include_once('khac/capnhatthongtinus.php') ?>
    </section>
