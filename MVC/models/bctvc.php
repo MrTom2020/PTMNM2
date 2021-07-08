d517d4dbf427fd92e8b73f1cc73e6c1f3dbd0d41
MrTom2020 <tomhumchin2@gmail.com>
2021-07-09 01:04:19 +0700

    make it better

--- MVC/models/bctvc.php
+++ MVC/models/bctvc.php
@@ -6,6 +6,7 @@
             $kq = "123";
             $sql = "CALL baocaotongquan('$ID')";
             $row = mysqli_query($this->con,$sql);
+            $kq = $row;
             while($row2 = mysqli_fetch_array($row))
            {
                $kq = "12345678";
