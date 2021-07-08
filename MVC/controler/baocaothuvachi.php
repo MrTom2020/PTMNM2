87dde2998257d4d037022f7411a8c657fd04b4cd
MrTom2020 <tomhumchin2@gmail.com>
2021-07-09 00:36:38 +0700

    make it better

--- MVC/controler/baocaothuvachi.php
+++ MVC/controler/baocaothuvachi.php
@@ -17,9 +17,6 @@
            // $id = isset($_POST['tenvi']) ? $_POST['tenvi']:"";
             $un3 = isset($_POST['un3']) ? $_POST['un3']:"";
            echo $this->a->bc($un3);
-           $this->view("tintuc",[
-            "Page"=>"baocaothuchi",
-        ]);
     }
 }
     
