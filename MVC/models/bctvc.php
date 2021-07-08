145a3b6dc61f40321dcbd200f6ac2ee846462220
MrTom2020 <tomhumchin2@gmail.com>
2021-07-09 01:02:11 +0700

    make it better

--- MVC/models/bctvc.php
+++ MVC/models/bctvc.php
@@ -8,7 +8,7 @@
             $row = mysqli_query($this->con,$sql);
             while($row2 = mysqli_fetch_array($row))
            {
-               $kq = $row2[1];
+               $kq = "12345678";
            }
             return $kq;
         }
