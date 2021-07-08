a89b8f0d46ba5cd219fe67b91be8fcf8c13b9e95
MrTom2020 <tomhumchin2@gmail.com>
2021-07-09 00:48:37 +0700

    make it better

--- MVC/models/bctvc.php
+++ MVC/models/bctvc.php
@@ -6,7 +6,7 @@
             $kq = "A hu hu";
             $sql = "CALL baocaotongquan('$ID')";
             $row = mysqli_query($this->con,$sql);
-            $kq = $row[1];
+            $kq = $row['Ten'];
             return $kq;
         }
     }
