f0a9e3511f1211fd9eee4e763d1ce858e19900ba
MrTom2020 <tomhumchin2@gmail.com>
2021-07-09 22:08:36 +0700

    make it better

--- MVC/controler/baocaothuvachi.php
+++ MVC/controler/baocaothuvachi.php
@@ -66,7 +66,8 @@
             }
             </script>
             </head>
-            <div id='chartContainer' style='height: 370px; width: 45%;margin-top:6vh;'></div>";
+            <div id='chartContainer' style='height: 370px; width: 45%;margin-top:6vh;'></div>
+            <script src='https://canvasjs.com/assets/script/canvasjs.min.js'></script>";
     }
 }
     
