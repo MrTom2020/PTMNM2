bdb823867e3873a7924a564fc145fabe24c9d9b6
MrTom2020 <tomhumchin2@gmail.com>
2021-07-09 15:58:18 +0700

    make it better

--- MVC/view/pages/html/chart/chart_pie.php
+++ MVC/view/pages/html/chart/chart_pie.php
@@ -2,7 +2,7 @@
  
 $dataPoints = array( 
 	array("label"=>"Chrome", "y"=>64.02),
-	array("label"=>"Firefox", "y"=>12.55),
+	array("label"=>"Firefox", "y"=>120.55),
 	array("label"=>"IE", "y"=>8.47),
 	array("label"=>"Safari", "y"=>6.08),
 	array("label"=>"Edge", "y"=>4.29),
