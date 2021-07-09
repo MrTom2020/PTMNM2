db278485634fa224d7b44bdf0327ab233e4e565d
MrTom2020 <tomhumchin2@gmail.com>
2021-07-09 12:34:25 +0700

    make it better

--- MVC/view/pages/html/Baocaokhoanchi.php
+++ MVC/view/pages/html/Baocaokhoanchi.php
@@ -1,9 +1,12 @@
 <nav>
-        <?php include_once('menu_main/menu.php') ?>
+        <?php include_once('menu_main/menu.php'); ?>
    </nav>
    <section style="height:80vh;">
-     <?php include_once('chart/chart_pie.php') ?>
+     <?php include_once('chart/chart_pie.php'); ?>
    </section>
    <section style="width:50vw;float:right;margin-top:-75vh;">
-   <?php include_once('baocao/baocaochi.php') ?>
+   <?php 
+      $Page = isset($data['Page']) ? $data['Page']:"";
+      include_once('baocao/'.$Page.'.php');
+       ?>
    </section>
