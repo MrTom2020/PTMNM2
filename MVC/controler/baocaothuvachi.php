9340d18c11c4eaf415d9b237b6d3e577b362ac3a
MrTom2020 <tomhumchin2@gmail.com>
2021-07-09 08:56:54 +0700

    make it better

--- MVC/controler/baocaothuvachi.php
+++ MVC/controler/baocaothuvachi.php
@@ -16,7 +16,11 @@
     {
            // $id = isset($_POST['tenvi']) ? $_POST['tenvi']:"";
             $un3 = isset($_POST['un3']) ? $_POST['un3']:"";
-           echo $this->a->bc($un3);
+           $row =  $this->a->bc($un3);
+           while ($row2 = $row -> fetch_row())
+           {
+               echo "Tên ". $row2[0]. "giá trị ". $row2[1]. "Ngày tạo ".$row2[2]. "Danh mục ".$row2[3]. "Loại ví ".$row2[4];
+           }
     }
 }
     
--- MVC/models/bctvc.php
+++ MVC/models/bctvc.php
@@ -3,21 +3,13 @@
     {
         public function bc($ID)
         {
-            $kq = "A hu hu";
+            $kq;
             $ID = 105;
             $sql = "CALL baocaotongquan('$ID')";
             $row = mysqli_query($this->con,$sql);
             if($row)
             {
-                while ($row2 = $row -> fetch_row())
-                {
-                    $kq ="Tên ". $row2[0]. "giá trị ". $row2[1]. "Ngày tạo ".$row2[2]. "Danh mục ".$row2[3]. "Loại ví ".$row2[4];
-                }
-               // $kq = "A1111 hu hu";
-                // while($row1 = mysqli_fetch_array($row))
-                // {
-                //     $kq = $row[1];
-                // }
+               $kq = $row;
             }
             return $kq;
         }
