74a8d8b5f7b0d8028767b1d0d379b543e0ce0876
MrTom2020 <tomhumchin2@gmail.com>
2021-07-11 19:11:46 +0700

    make it better

--- MVC/view/pages/html/chart/chart_dt.php
+++ MVC/view/pages/html/chart/chart_dt.php
@@ -1,9 +1,9 @@
 <?php 
 $datab = isset($data2['ttc']) ? $data2['ttc']:NULL;
-$kq = $datab -> fetch_row();
- $dataa  = isset($data['bd']) ? $data['bd']:NULL;
+$kq[0] = $datab -> fetch_row();
+// $dataa  = isset($data['bd']) ? $data['bd']:NULL;
 $dataPoints = array( 
-	array("label"=>"Chrome", "y"=>$dataa),
+	array("label"=>"Chrome", "y"=>$kq),
 	array("label"=>"Firefox", "y"=>12.55),
 	array("label"=>"IE", "y"=>8.47),
 	array("label"=>"Safari", "y"=>6.08),
