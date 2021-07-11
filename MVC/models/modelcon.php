8b6fea4a58a2746e4f43b0f78ddc77b4981a5dc9
MrTom2020 <tomhumchin2@gmail.com>
2021-07-11 20:24:14 +0700

    make it better

--- MVC/models/modelcon.php
+++ MVC/models/modelcon.php
@@ -57,11 +57,13 @@
         $sql = "CALL tongtiencuauser('$ID')";
         $row = mysqli_query($this->con,$sql);
         $kq = $row -> fetch_row();
+        $this->con->clone();
         return $kq[0];
 
     }
     public function tongtienc($ID)
     {
+        $this->con->clone();
         $kq5;
         $sql5 = "CALL tongtienchi('$ID')";
         $row5 = mysqli_query($this->con,$sql5);
