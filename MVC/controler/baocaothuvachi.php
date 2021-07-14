3dc45e02e2d05e23686315af5e29930bc51d5a77
MrTom2020 <tomhumchin2@gmail.com>
2021-07-15 01:01:53 +0700

    make it better

--- MVC/controler/baocaothuvachi.php
+++ MVC/controler/baocaothuvachi.php
@@ -15,6 +15,7 @@
     public function baocaotq()
     {
            // $id = isset($_POST['tenvi']) ? $_POST['tenvi']:"";
+          $tc = isset($_SESSION['listienchi']) ? $_SESSION['listienchi']:1;
         $un3 = isset($_POST['un3']) ? $_POST['un3']:"";
         $row =  $this->a->bc($un3);
            $_SESSION['bdtq'] = $this->a->bieudotongquat($un3);
@@ -23,8 +24,8 @@
            <tr>
               <th scope='col'>Tên ví</th>
               <th scope='col'>Tiền trong ví</th>
-              <th scope='col'>Danh mục</th>
               <th scope='col'>Loại</th>
+              <th scope='col'>Chiếm</th>
               <th scope='col'>Ngày tạo</th>
               <th scope='col'>Xóa</th>
           </tr>
@@ -33,12 +34,13 @@
            while ($row2 = $row -> fetch_row())
            {
             $tg = $row2[2];
+            $t = ($row2[1] / $tc) * 100;
             $k = date("d/m/Y",strtotime($tg));
              echo "<tr>
              <th scope='row'>$row2[0]</th>
              <td>$row2[1]</td>
-             <td>$row2[3]</td>
              <td>$row2[4]</td>
+             <td>$t</td>
              <td>$k</td>
              <td><img src='https://img.icons8.com/ios/50/000000/delete--v3.png'/></td>
              </tr>";
