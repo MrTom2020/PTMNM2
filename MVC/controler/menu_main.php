5cc51eaf95731fd4c97ffddb6e68082484d3d870
MrTom2020 <tomhumchin2@gmail.com>
2021-07-15 00:48:58 +0700

    make it better

--- MVC/controler/menu_main.php
+++ MVC/controler/menu_main.php
@@ -79,10 +79,10 @@
             if(isset($_POST['gt13']))
             {
                 $ID = isset($_SESSION['iduu']) ? $_SESSION['iduu']:"";
-                $_SESSION['listienchi'] = $this->c->dstchi();
-                $_SESSION['listienthu'] = $this->d->dstthu();
-               // $_SESSION['listiendt'] = $this->e->dstdt();
-                $_SESSION['tongtc'] = $this->modelcon->tongtien($ID) + $this->b->tongtienc($ID);//$this->modelcon->tongtienc($ID);
+            //     $_SESSION['listienchi'] = $this->c->dstchi();
+            //     $_SESSION['listienthu'] = $this->d->dstthu();
+            //    // $_SESSION['listiendt'] = $this->e->dstdt();
+            //     $_SESSION['tongtc'] = $this->modelcon->tongtien($ID) + $this->b->tongtienc($ID);//$this->modelcon->tongtienc($ID);
                 $this->view("tintuc",[
                     "Page"=>"Baocaokhoanchi",
                     "Page1"=>"baocaodautu",
