0421eb65c3802cc4e654f545aba0e1e266d97e84
MrTom2020 <tomhumchin2@gmail.com>
2021-07-14 23:07:24 +0700

    make it better

--- MVC/view/pages/html/chart/chart_dt.php
+++ MVC/view/pages/html/chart/chart_dt.php
@@ -10,9 +10,9 @@ $k = 10;
 		$tongtc +=$row[2];
 	}
  }
-
+$ptc = ($tongtc/$datab) * 100 * -1;
 $dataPoints = array( 
-	array("label"=>"Chrome", "y"=>$tongtc),
+	array("label"=>"Chrome", "y"=>$ptc),
 	array("label"=>"Firefox", "y"=>12.55),
 	array("label"=>"IE", "y"=>8.47),
 	array("label"=>"Safari", "y"=>6.08),
