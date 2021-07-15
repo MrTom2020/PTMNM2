6fcbfbc882cae52e6638ce2b014786f92cb43a3a
MrTom2020 <tomhumchin2@gmail.com>
2021-07-16 00:54:54 +0700

    make it better

--- MVC/controler/baocaothuvachi.php
+++ MVC/controler/baocaothuvachi.php
@@ -23,7 +23,7 @@
     public function baocaotq()
     {
            // $id = isset($_POST['tenvi']) ? $_POST['tenvi']:"";
-       // $ID = isset($_SESSION['iduu']) ? $_SESSION['iduu']:"";
+        $ID = isset($_SESSION['iduu']) ? $_SESSION['iduu']:"";
        // $tongtien = $this->modelcon->tongtien($ID) + $this->b->tongtienc($ID);
         $un3 = isset($_POST['un3']) ? $_POST['un3']:"";
         // $tientv = ($this->d->ttchi($un3) * -1);
