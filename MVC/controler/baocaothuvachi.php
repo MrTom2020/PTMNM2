c3ecf11b54bb99c1506891e1e6e59aa472e4b8de
MrTom2020 <tomhumchin2@gmail.com>
2021-07-16 01:29:50 +0700

    make it better

--- MVC/controler/baocaothuvachi.php
+++ MVC/controler/baocaothuvachi.php
@@ -24,11 +24,11 @@
     {
            // $id = isset($_POST['tenvi']) ? $_POST['tenvi']:"";
         $ID = isset($_SESSION['iduu']) ? $_SESSION['iduu']:"";
-       // $tongtien = $this->modelcon->tongtien($ID) + $this->b->tongtienc($ID);
+        $tongtien = $this->modelcon->tongtien($ID) + $this->b->tongtienc($ID);
         $un3 = isset($_POST['un3']) ? $_POST['un3']:"";
         // $tientv = ($this->d->ttchi($un3) * -1);
         //$pt = ($tientv/$tongtien) * 100;
-        echo  $this->d->ttchi(115);
+         echo $tongtien; //$this->d->ttchi(115);
         $row =  $this->a->bc($un3);
            $_SESSION['bdtq'] = $this->a->bieudotongquat($un3);
            echo "<table class='table'>";
