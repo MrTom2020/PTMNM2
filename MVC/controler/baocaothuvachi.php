e8f9dbac1f93a16537100ed4b8218c6ec13ff288
MrTom2020 <tomhumchin2@gmail.com>
2021-07-15 22:35:28 +0700

    make it better

--- MVC/controler/baocaothuvachi.php
+++ MVC/controler/baocaothuvachi.php
@@ -26,8 +26,8 @@
         $ID = isset($_SESSION['iduu']) ? $_SESSION['iduu']:"";
         $tongtien = $this->modelcon->tongtien($ID) + $this->b->tongtienc($ID);
         $un3 = isset($_POST['un3']) ? $_POST['un3']:"";
-       // $tientv = $this->c->ttv($un3) + ($this->d->ttchi($un3) * -1);
-        $pt = 100;//($tientv/$tongtien) * 100;
+        $tientv = $this->c->ttv($un3) + ($this->d->ttchi($un3) * -1);
+        $pt = ($tientv/$tongtien) * 100;
         $row =  $this->a->bc($un3);
            $_SESSION['bdtq'] = $this->a->bieudotongquat($un3);
            echo "<table class='table'>";
