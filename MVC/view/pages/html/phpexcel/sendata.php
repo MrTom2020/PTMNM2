73afc667abbd8d9676fd988d0e1d881eef046912
MrTom2020 <tomhumchin2@gmail.com>
2021-07-16 16:57:31 +0700

    make it better

--- MVC/view/pages/html/phpexcel/sendata.php
+++ MVC/view/pages/html/phpexcel/sendata.php
@@ -1,23 +1,4 @@
-<script>
-    $(function() {
-    $("#exporttable").click(function(e){
-    var table = $("#htmltable");
-    if(table && table.length){
-    $(table).table2excel({
-    exclude: ".noExl",
-    name: "Excel Document Name",
-    filename: "BBBootstrap" + new Date().toISOString().replace(/[\-\:\.]/g, "") + ".xls",
-    fileext: ".xls",
-    exclude_img: true,
-    exclude_links: true,
-    exclude_inputs: true,
-    preserveColors: false
-    });
-    }
-    });
-    
-    });
-</script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/pages/js/sendata.js"/>
 <div class="page-content page-container" id="page-content">
     <div class="padding">
         <div class="row container d-flex justify-content-center">
--- MVC/view/tintuc.php
+++ MVC/view/tintuc.php
@@ -16,7 +16,6 @@
 <folder>
     <?php include_once('MVC/view/pages/html/folder/folder.php') ?>
 </folder>
-
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"/>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
