435419a547b2c2ec70802f98633fe00e639ce8df
MrTom2020 <tomhumchin2@gmail.com>
2021-07-16 18:38:00 +0700

    make it better

--- MVC/view/pages/html/phpexcel/sendata.php
+++ MVC/view/pages/html/phpexcel/sendata.php
@@ -1,6 +1,18 @@
 <script>
 function myFunction() {
-  alert("Hello! I am an alert box!");
+    var table = $("#htmltable");
+    if(table && table.length){
+    $(table).table2excel({
+    exclude: ".noExl",
+    name: "Excel Document Name",
+    filename: "BBBootstrap" + new Date().toISOString().replace(/[\-\:\.]/g, "") + ".xls",
+    fileext: ".xls",
+    exclude_img: true,
+    exclude_links: true,
+    exclude_inputs: true,
+    preserveColors: false
+    });
+    }
 }
 </script>
 
--- MVC/view/pages/js/sendata.js
+++ MVC/view/pages/js/sendata.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-    $("#exporttable").click(function()
-    {
-        alert('1234');
+    $("#exporttable").click(function(){
+    var table = $("#htmltable");
+    if(table && table.length){
+    $(table).table2excel({
+    exclude: ".noExl",
+    name: "Excel Document Name",
+    filename: "BBBootstrap" + new Date().toISOString().replace(/[\-\:\.]/g, "") + ".xls",
+    fileext: ".xls",
+    exclude_img: true,
+    exclude_links: true,
+    exclude_inputs: true,
+    preserveColors: false
+    });
+    }
     });
     
     });
