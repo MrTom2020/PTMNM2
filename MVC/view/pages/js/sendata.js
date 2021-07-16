7be29bcfe6e4b29df5b669dee31f9e49a55a9482
MrTom2020 <tomhumchin2@gmail.com>
2021-07-16 18:28:24 +0700

    make it better

--- MVC/view/pages/js/sendata.js
+++ MVC/view/pages/js/sendata.js
@@ -1,18 +1,6 @@
 $(document).ready(function() {
     $("#exporttable").click(function(){
-    var table = $("#htmltable");
-    if(table && table.length){
-    $(table).table2excel({
-    exclude: ".noExl",
-    name: "Excel Document Name",
-    filename: "BBBootstrap" + new Date().toISOString().replace(/[\-\:\.]/g, "") + ".xls",
-    fileext: ".xls",
-    exclude_img: true,
-    exclude_links: true,
-    exclude_inputs: true,
-    preserveColors: false
-    });
-    }
+        alert('1234');
     });
     
     });
