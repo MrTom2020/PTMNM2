3abef3132b49eb73f2632d7e7a77d7025a78aace
MrTom2020 <tomhumchin2@gmail.com>
2021-07-16 18:16:44 +0700

    make it better

--- MVC/view/sendata.php
+++ MVC/view/sendata.php
@@ -16,8 +16,8 @@
 <folder>
     <?php include_once('MVC/view/pages/html/folder/folder.php') ?>
 </folder>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"/>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 <script src="https://quanlychitieu2030.herokuapp.com/MVC/view/pages/js/sendata.js" crossorigin="anonymous"/>
 </body>
 </html>
