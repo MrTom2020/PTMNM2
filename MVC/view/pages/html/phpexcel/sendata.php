c615fedabb0902e4edae0dd8ba68c7823f187f55
MrTom2020 <tomhumchin2@gmail.com>
2021-07-16 18:51:26 +0700

    make it better

--- MVC/view/pages/html/phpexcel/sendata.php
+++ MVC/view/pages/html/phpexcel/sendata.php
@@ -32,27 +32,65 @@ function exportTableToExcel(tableID, filename = ''){
 </script>
 
 <div class="page-content page-container" id="page-content">
-<table id="tblData">
-    <tr>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Country</th>
-    </tr>
-    <tr>
-        <td>John Doe</td>
-        <td>john@gmail.com</td>
-        <td>USA</td>
-    </tr>
-    <tr>
-        <td>Michael Addison</td>
-        <td>michael@gmail.com</td>
-        <td>UK</td>
-    </tr>
-    <tr>
-        <td>Sam Farmer</td>
-        <td>sam@gmail.com</td>
-        <td>France</td>
-    </tr>
-</table>
-<button onclick="exportTableToExcel('tblData')">Export Table Data To Excel File</button>
+    <div class="padding">
+        <div class="row container d-flex justify-content-center">
+            <div class="col-lg-8 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <h4 class="card-title">Basic Table</h4>
+                                <p class="card-description"> Basic table with card </p>
+                            </div>
+                            <div class="col-md-4 text-right"> <button onclick="exportTableToExcel('htmltable')">Export Table Data To Excel File</button>
+                        </div>
+                        <div class="table-responsive">
+                            <table id="htmltable" class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>ID No.</th>
+                                        <th>Created On</th>
+                                        <th>Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Samso Park</td>
+                                        <td>34424433</td>
+                                        <td>12 May 2017</td>
+                                        <td><label class="badge badge-danger">Pending</label></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Marlo Sanki</td>
+                                        <td>53425532</td>
+                                        <td>15 May 2015</td>
+                                        <td><label class="badge badge-warning">In progress</label></td>
+                                    </tr>
+                                    <tr>
+                                        <td>John ryte</td>
+                                        <td>53275533</td>
+                                        <td>14 May 2017</td>
+                                        <td><label class="badge badge-info">Fixed</label></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Peter mark</td>
+                                        <td>53275534</td>
+                                        <td>16 May 2017</td>
+                                        <td><label class="badge badge-success">Completed</label></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Dave</td>
+                                        <td>53275535</td>
+                                        <td>20 May 2017</td>
+                                        <td><label class="badge badge-warning">In progress</label></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
