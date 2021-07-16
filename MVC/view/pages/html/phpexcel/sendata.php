3b9df4bbe56120b5afb846faab2224ba39b920c7
MrTom2020 <tomhumchin2@gmail.com>
2021-07-16 16:39:31 +0700

    make it better

--- MVC/view/pages/html/phpexcel/sendata.php
+++ MVC/view/pages/html/phpexcel/sendata.php
@@ -1,3 +1,23 @@
+<script>
+    $(function() {
+    $("#exporttable").click(function(e){
+    var table = $("#htmltable");
+    if(table && table.length){
+    $(table).table2excel({
+    exclude: ".noExl",
+    name: "Excel Document Name",
+    filename: "BBBootstrap" + new Date().toISOString().replace(/[\-\:\.]/g, "") + ".xls",
+    fileext: ".xls",
+    exclude_img: true,
+    exclude_links: true,
+    exclude_inputs: true,
+    preserveColors: false
+    });
+    }
+    });
+    
+    });
+</script>
 <div class="page-content page-container" id="page-content">
     <div class="padding">
         <div class="row container d-flex justify-content-center">
