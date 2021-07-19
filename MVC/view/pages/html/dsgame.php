b4c2061df906ae414e16c2724c5f15b720496fe1
MrTom2020 <tomhumchin2@gmail.com>
2021-07-20 01:39:58 +0700

    make it better

--- MVC/view/pages/html/dsgame.php
+++ MVC/view/pages/html/dsgame.php
@@ -1,7 +1,7 @@
 <nav>
         <?php include_once('menu_main/menu.php') ?>
    </nav>
-   <section style="height:80vh;">
+   <section style="height:80vh;width:90vw;">
      <?php include_once('dsgame/gamebantim.php') ?>
    </section>
    
