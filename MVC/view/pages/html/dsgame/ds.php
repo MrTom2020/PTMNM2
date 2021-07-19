01e3ddf137f2629652d71360970b17ba0ce0923b
MrTom2020 <tomhumchin2@gmail.com>
2021-07-20 01:47:13 +0700

    make it better

---
+++ MVC/view/pages/html/dsgame/ds.php
@@ -0,0 +1,51 @@
+<form  action="../quanlytaikhoan/ql" method="POST">
+<div class="max-width text-center middle-things">
+<h3 class="text-center text-up green visible-xs-block visible-sm-block" style="margin-bottom:10vh;">Hi !<?php echo isset($_SESSION['ten']) ?  $_SESSION['ten']:"" ?></h3>
+<div class="row">
+<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn" style="margin-bottom:5vh;">
+<div class="animated features-ml">
+<img src="https://img.icons8.com/ios-filled/50/26e07f/wallet.png"/>
+<h3 class="fourthlabel">Amazing wallet</h3>
+<input type="submit" class="form-control"  name="vitien" id="vitien" value="Amazing wallet là công cụ tạo ví">
+</div>
+</div>
+<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
+<div class="animated features-ml">
+<img src="https://img.icons8.com/ios-filled/50/26e07f/wallet.png"/>
+<h3 class="fourthlabel">Amazing list wallet</h3>
+<input type="submit" class="form-control"  name="danhsachvi" id="danhsachvi" value="Danh sách ví">
+</div>
+</div>
+<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
+<div class="animated features-ml">
+<img src="https://img.icons8.com/ios-filled/50/26e07f/wallet.png"/>
+<h3 class="fourthlabel">Amazing member wallet</h3>
+<input type="submit" class="form-control"  name="cd" id="cd" value="Ví thành viên.">
+</div>
+</div>
+
+
+<div class="col-md-4 col-lg-4  col-xs-6 animated fadeIn" style="margin-bottom:5vh;">
+<div class="animated features-ml">
+<img src="https://img.icons8.com/ios-filled/50/26e07f/wallet.png"/>
+<h3 class="fourthlabel">Amazing update wallet</h3>
+<input type="submit" class="form-control"  name="capnhatvi" id="capnhatvi" value="Cập nhật ví.">
+</div>
+</div>
+<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
+<div class="animated features-ml">
+<img src="https://img.icons8.com/ios-filled/50/26e07f/group-background-selected.png"/>
+<h3 class="fourthlabel">Amazing personal information</h3>
+<input type="submit" class="form-control"  name="capnhatthongtincn" id="capnhatthongtincn" value="Cập nhật thông tin.">
+</div>
+</div>
+<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
+<div class="animated features-ml">
+<img src="https://img.icons8.com/ios-filled/50/26e07f/group-background-selected.png"/>
+<h3 class="fourthlabel">Danh sách tài khoản thành viên</h3>
+<input type="submit" class="form-control"  name="danhsachtk" id="danhsachtk" value="Danh sách thành viên.">
+</div>
+</div>
+</div>
+</div>
+</form>
