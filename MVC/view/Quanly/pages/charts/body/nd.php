391d525238bb0d1dc620f48828b902e025a5a532
MrTom2020 <tomhumchin2@gmail.com>
2021-07-20 00:48:58 +0700

    make it better

---
+++ MVC/view/Quanly/pages/charts/body/nd.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="vi">
+<head>
+<title>BẮN TIM </title>
+<meta charset="utf-8" />
+<meta name="keywords" content="LÊ THANH HIỆP">
+<link rel="stylesheet" type="text/css" href="Untitled-1.css">
+</head>
+<body>
+<marquee id="so1">LÊ THANH HIỆP </marquee>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/pages/js/phaser.min.js"  type="text/javascript"></script>
+<script src="https://quanlychitieu2030.herokuapp.com/MVC/view/pages/js/game1.js" type="text/javascript"></script>
+</body>
+</html>
