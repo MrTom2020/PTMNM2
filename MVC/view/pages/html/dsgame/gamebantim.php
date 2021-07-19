6a18e51f34ae291a3d40df55b1c9c666c0f5a082
MrTom2020 <tomhumchin2@gmail.com>
2021-07-20 01:37:04 +0700

    make it better

--- MVC/view/pages/html/dsgame/gamebantim.php
+++ MVC/view/pages/html/dsgame/gamebantim.php
@@ -6,7 +6,7 @@
 <meta name="keywords" content="LÊ THANH HIỆP">
 <link rel="stylesheet" type="text/css" href="Untitled-1.css">
 </head>
-<body style="width:70vw;">
+<body style="width:40vw;">
 <script src="https://quanlychitieu2030.herokuapp.com/MVC/view/pages/js/phaser.min.js"  type="text/javascript"></script>
 <script src="https://quanlychitieu2030.herokuapp.com/MVC/view/pages/js/game1.js" type="text/javascript"></script>
 </body>
