b6a0b7566b76391d3898567b5b35c232d7aa0f02
MrTom2020 <tomhumchin2@gmail.com>
2021-07-20 17:18:09 +0700

    make it better

--- MVC/controler/Quanlydichvu.php
+++ MVC/controler/Quanlydichvu.php
@@ -4,7 +4,7 @@
        public $modelcon;
        public function __construct()
        {
-          $this->modelcon = $this->model("Qlnd");
+         // $this->modelcon = $this->model("Qlnd");
        }
        function SayHi()
        {
@@ -15,13 +15,18 @@
        }
        function themdv()
        {
-        if(isset($_POST["a1"]))
+        if(isset($_POST["dongythem"]))
         {
-            $this->view("Admin",[
-                "Page"=>"HomeAdmin",
-                "Page1"=>"index",
-                "dsnd"=>$this->modelcon->ds()
-            ]);
+            // $this->view("Admin",[
+            //     "Page"=>"HomeAdmin",
+            //     "Page1"=>"index",
+            //     "dsnd"=>$this->modelcon->ds()
+            // ]);
+            $tendv = isset($_POST["tendv"]) ? $_POST["tendv"]:"";
+            $tenfile = isset($_POST["tenfile"]) ? $_POST["tenfile"]:"";
+            $loai = isset($_POST["loaidv"]) ? $_POST["loaidv"]:"";
+            $noidung = isset($_POST["noidungdv"]) ? $_POST["noidungdv"]:"";
+            echo $tendv ."|". $tenfile ."|". $loai ."|". $noidung;
         }
        }
     }
