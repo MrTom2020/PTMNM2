bed4c86bfc090340d7a310823ff2e34a4ad5f8ee
MrTom2020 <tomhumchin2@gmail.com>
2021-07-21 00:03:50 +0700

    make it better

--- MVC/view/pages/html/khac/game.php
+++ MVC/view/pages/html/khac/game.php
@@ -1,11 +1,18 @@
 <?php
     $ds = isset($data["dsgame"]) ? $data["dsgame"]:NULL;
+    echo "<div class='row'>";
     if($ds)
     {
         while($row = mysqli_fetch_array($ds))
         {
-            echo $row[1];
+           echo "<div class='col-md-4 col-lg-4 col-xs-6 animated fadeIn' style='margin-bottom:5vh;'>
+           <div class='animated features-ml'>
+           <img src='https://img.icons8.com/emoji/50/000000/video-game-emoji.png'/>
+           <h3 class='fourthlabel'>$row[1]</h3>
+           <input type='submit' class='form-control'  name='dsgame' id='dsgame' value='$row[4]'>
+           </div>
+           </div>";
         }
-        echo '123333';
     }
+    echo "</div>";
  ?>
