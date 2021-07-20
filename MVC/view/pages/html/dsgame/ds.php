1695a9ff027feacbb5ed6a9b4e5204b11a774997
MrTom2020 <tomhumchin2@gmail.com>
2021-07-20 15:48:57 +0700

    make it better

--- MVC/view/pages/html/dsgame/ds.php
+++ MVC/view/pages/html/dsgame/ds.php
@@ -6,21 +6,21 @@
 <div class="animated features-ml">
 <img src="https://img.icons8.com/emoji/50/000000/video-game-emoji.png"/>
 <h3 class="fourthlabel">Danh sách game</h3>
-<input type="submit" class="form-control"  name="vitien" id="vitien" value="Amazing wallet là công cụ tạo ví">
+<input type="submit" class="form-control"  name="vitien" id="vitien" value="Chơi là có thưởng">
 </div>
 </div>
 <div class="col-md-4 col-lg-4  col-xs-6 animated fadeIn" style="margin-bottom:5vh;">
 <div class="animated features-ml">
 <img src="https://img.icons8.com/fluent/50/000000/file.png"/>
 <h3 class="fourthlabel">Xuất file csv</h3>
-<input type="submit" class="form-control"  name="capnhatvi" id="capnhatvi" value="Cập nhật ví.">
+<input type="submit" class="form-control"  name="capnhatvi" id="capnhatvi" value="Tạo ra file csv">
 </div>
 </div>
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
 <div class="animated features-ml">
 <img src="https://img.icons8.com/ios-filled/50/26e07f/group-background-selected.png"/>
 <h3 class="fourthlabel">Nhập dữ liệu từ Excel</h3>
-<input type="submit" class="form-control"  name="capnhatthongtincn" id="capnhatthongtincn" value="Cập nhật thông tin.">
+<input type="submit" class="form-control"  name="capnhatthongtincn" id="capnhatthongtincn" value="Nhập dữ liệu từ bên ngoài">
 </div>
 </div>
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
