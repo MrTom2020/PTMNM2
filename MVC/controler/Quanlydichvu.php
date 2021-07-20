30e78ffb37db9b83bf453edb1f0569cd2bbbf569
MrTom2020 <tomhumchin2@gmail.com>
2021-07-20 22:19:24 +0700

    make it better

--- MVC/controler/Quanlydichvu.php
+++ MVC/controler/Quanlydichvu.php
@@ -17,16 +17,16 @@
        {
         if(isset($_POST["dongythem"]))
         {
-            // $this->view("Admin",[
-            //     "Page"=>"HomeAdmin",
-            //     "Page1"=>"index",
-            //     "dsnd"=>$this->modelcon->ds()
-            // ]);
             $tendv = isset($_POST["tendv"]) ? $_POST["tendv"]:"";
             $tenfile = isset($_POST["tenfile"]) ? $_POST["tenfile"]:"";
             $loai = isset($_POST["loaidv"]) ? $_POST["loaidv"]:"";
             $noidung = isset($_POST["noidungdv"]) ? $_POST["noidungdv"]:"";
-           echo $this->themdv->themdvu($tendv,$loai,$noidung, $tenfile);
+           $this->themdv->themdvu($tendv,$loai,$noidung, $tenfile);
+             $this->view("Admin",[
+            "Page"=>"HomeAdmin",
+            "Page1"=>"themdv"
+              ]);
+
         }
        }
     }
