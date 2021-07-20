14e32fcf4cf3307749dada64d6b3c4142e24fdb7
MrTom2020 <tomhumchin2@gmail.com>
2021-07-20 21:48:07 +0700

    make it better

--- MVC/models/themdichvu.php
+++ MVC/models/themdichvu.php
@@ -1,14 +1,14 @@
 <?php
     class themdichvu extends DB
     {
-        public function themdvu()
+        public function themdvu($tendichv,$loai,$noidung, $tenfile)
         {
             $kk = "Thêm không thành công";
-            // $sql = "CALL themdichvu('$tendichv','$loai','$noidung', '$tenfile')";
-            // if(mysqli_query($this->con,$sql))
-            // {
-            //     $kk = "Thêm thành công";
-            // }
+            $sql = "CALL themdichvu('$tendichv','$loai','$noidung', '$tenfile')";
+            if(mysqli_query($this->con,$sql))
+            {
+                $kk = "Thêm thành công";
+            }
             return $kk;
         }
     }
