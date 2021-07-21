fcdf7f3120cdf191a46a681ccccecc0e9625d07d
MrTom2020 <tomhumchin2@gmail.com>
2021-07-21 15:07:41 +0700

    make it better

---
+++ MVC/controler/dsgamee.php
@@ -0,0 +1,28 @@
+<?php
+    class dsgameee extends Controller
+    {
+        public $a;
+        public function __construct()
+        {
+          //  $this->a = $this->model("modelcon");
+        }
+    public function SayHi()
+    {
+        $this->view("tintuc",[
+            "Page"=>"game",
+        ]);
+    }
+    public function nsk()
+    {
+        if(isset($_POST["game1"]))
+        {
+            $this->view("main",[
+                "Page"=>"main",
+            ]);
+        }
+    }
+
+   
+}
+    
+?>
