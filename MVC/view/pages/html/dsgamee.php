5704de71a2177bd56dcef2e13bd513924e1114dd
MrTom2020 <tomhumchin2@gmail.com>
2021-07-21 23:04:42 +0700

    make it better

--- MVC/view/pages/html/dsgamee.php
+++ MVC/view/pages/html/dsgamee.php
@@ -1,5 +1,5 @@
  <?php 
-    echo "<iframe src='https://www.nhaccuatui.com/mh/background/C08E4AiteDUm' width='1' height='1' frameborder='0' allowfullscreen allow='autoplay'></iframe>";
+    echo "<iframe src='https://www.nhaccuatui.com/mh/auto/SgXWolUyq8Dp' width='1' height='1' frameborder='0' allowfullscreen allow='autoplay'></iframe>";
     echo "<form  action='../Dichvu/dvu' method='POST'>
     <input type='submit' class='form-control'  name='qlgame' id='qlgame' value='Quay lại'>
     </from>";
