4e4bc1b0a9329c0db312391efab37dc98de5c934
MrTom2020 <tomhumchin2@gmail.com>
2021-07-21 17:52:06 +0700

    make it better

--- MVC/view/pages/js/game1.js
+++ MVC/view/pages/js/game1.js
@@ -1,5 +1,5 @@
 
-var game = new Phaser.Game(1520,800, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1510,750, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
 
 function preload() {
 
