011cfbb7f347909d88dcc3d15df5e01e59a6580d
MrTom2020 <tomhumchin2@gmail.com>
2021-07-21 21:25:03 +0700

    make it better

--- MVC/view/pages/js/SlidingPuzzle.js
+++ MVC/view/pages/js/SlidingPuzzle.js
@@ -10,7 +10,7 @@ var piecesGroup,
     shuffledIndexArray = [];
 
 function preload() {
-    game.load.spritesheet("background", "https://quanlychitieu2030.herokuapp.com/MVC/view/pages/img/khac/bk1.jpg", PIECE_WIDTH, PIECE_HEIGHT);
+    game.load.spritesheet("background", "assets/games/sliding-puzzle/bl.jpg", PIECE_WIDTH, PIECE_HEIGHT);
 }
 
 function create() {
