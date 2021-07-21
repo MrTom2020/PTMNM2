29713fe77a78e102ed7499fa48799689d6211c73
MrTom2020 <tomhumchin2@gmail.com>
2021-07-21 08:21:29 +0700

    make it better

--- MVC/view/pages/html/khac/game.php
+++ MVC/view/pages/html/khac/game.php
@@ -7,7 +7,7 @@
         echo "<form  action='dsgamee/nsk' method='POST'>";
         while($row = mysqli_fetch_array($ds))
         {
-           echo "<div class='col-md-4 col-lg-4 col-xs-6 animated fadeIn' style='margin-bottom:5vh;'>
+           echo "<div class='col-md-3 col-lg-4 col-xs-6 animated fadeIn' style='margin-bottom:5vh;'>
            <div class='animated features-ml'>
            <img src='https://img.icons8.com/emoji/50/000000/video-game-emoji.png'/>
            <h3 class='fourthlabel'>$row[1]</h3>
