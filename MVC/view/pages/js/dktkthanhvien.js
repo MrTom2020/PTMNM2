18de6db51da8adf15dd1238803508e59170cedc3
MrTom2020 <tomhumchin2@gmail.com>
2021-07-23 03:22:11 +0700

    make it better

--- MVC/view/pages/js/dktkthanhvien.js
+++ MVC/view/pages/js/dktkthanhvien.js
@@ -4,9 +4,4 @@ $(document).ready(function(){
         var user = $(this).val();
         $.post("./../dangkytaikhoanthanhvien/kiemtradn",{un8:user},function(data){ $("#thongbao").html(data); });
     });
-    $("#form6Example5").keyup(function()
-    {
-        var user = $(this).val();
-        $.post("./../dangkytaikhoanthanhvien/kiemtradn",{un9:user},function(data){ $("#htkq2").html(data); });
-    });
 });
