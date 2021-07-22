79314173ffd952de2f649f656c4f72a5723195ad
MrTom2020 <tomhumchin2@gmail.com>
2021-07-23 02:46:05 +0700

    make it better

--- MVC/view/pages/html/vithanhvien.php
+++ MVC/view/pages/html/vithanhvien.php
@@ -2,5 +2,7 @@
         <?php include_once('menu_main/menu.php') ?>
    </nav>
    <section style="height:100vh;">
-     <?php include_once('khac/vithanhvien.php') ?>
+     <?php 
+     echo "123";
+     include_once('khac/vithanhvien.php') ?>
    </section>
