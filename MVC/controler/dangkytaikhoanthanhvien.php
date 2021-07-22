97c6e7beed25fb17fe2088760821819abbc7e26d
MrTom2020 <tomhumchin2@gmail.com>
2021-07-23 03:09:35 +0700

    make it better

--- MVC/controler/dangkytaikhoanthanhvien.php
+++ MVC/controler/dangkytaikhoanthanhvien.php
@@ -17,7 +17,7 @@
     public function kiemtradn()
     {
         $un8 = isset($_POST["un8"]) ? $_POST["un8"]:"";
-        echo $kq = $this->a->kt($un8);
+        echo  $this->$ktt->kt($un8);
     }
     public function khachdangkytaikhoantv()
     {
