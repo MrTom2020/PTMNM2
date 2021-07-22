5172dd313f32ff05c81e51feffce16c4b3f5cf3b
MrTom2020 <tomhumchin2@gmail.com>
2021-07-23 03:36:50 +0700

    make it better

--- MVC/view/pages/js/signup.js
+++ MVC/view/pages/js/signup.js
@@ -12,6 +12,6 @@ $(document).ready(function(){
     $("#hotentv").keyup(function()
     {
         var user = $(this).val();
-        $.post("./../dangkytaikhoanthanhvien/kiemtradn",{un8:user},function(data){ $("#form6Example1").html(data); });
+        $.post("../dangkytaikhoanthanhvien/kiemtradn",{un8:user},function(data){ $("#form6Example1").html(data); });
     });
 });
