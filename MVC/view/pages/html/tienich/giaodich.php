949ebfade788b042f43d442b2f69de43b114027c
MrTom2020 <tomhumchin2@gmail.com>
2021-07-23 01:44:12 +0700

    make it better

--- MVC/view/pages/html/tienich/giaodich.php
+++ MVC/view/pages/html/tienich/giaodich.php
@@ -22,23 +22,6 @@
 <input type="submit" class="form-control"  name="sdt" id="cd" value="Một công cụ ghi chép tiền đầu tư.">
 </div>
 </div>
-
-
-<div class="col-md-4 col-lg-4  col-xs-6 animated fadeIn" style="margin-bottom:5vh;">
-<div class="animated features-ml">
-<img src="https://img.icons8.com/dotty/71/26e07f/planner.png"/>
-<h3 class="fourthlabel">Sổ kế hoạch</h3>
-<input type="submit" class="form-control"  name="skh" id="useName" value="Một công cụ ghi chép kế hoạch.">
-</div>
-</div>
-<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
-<div class="animated features-ml">
-<img src="https://img.icons8.com/pastel-glyph/71/26e07f/note.png"/>
-<h3 class="fourthlabel">Sổ Nợ</h3>
-<input type="submit" class="form-control"  name="sn" id="useName" value="Một công cụ ghi chép tiền nợ">
-</div>
-</div>
-
 </div>
 </div>
 </form>
