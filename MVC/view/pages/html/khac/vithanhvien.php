51df74622a3c095bdec34e71344e6b650f4cb61c
MrTom2020 <tomhumchin2@gmail.com>
2021-07-23 02:53:06 +0700

    make it better

--- MVC/view/pages/html/khac/vithanhvien.php
+++ MVC/view/pages/html/khac/vithanhvien.php
@@ -7,8 +7,9 @@
       <th scope='col'>Tên ví</th>
       <th scope='col'>Tiền trong ví</th>
       <th scope='col'>Mã người dùng</th>
+      <th scope='col'>Tên người dùng</th>
+      <th scope='col'>Ngày tạo</th>
       <th scope='col'>Loại ví</th>
-      <th scope='col'>Xóa</th>
     </tr>
      </thead>";
      echo "<tbody>";
@@ -16,15 +17,16 @@
      {
      while($row = mysqli_fetch_array($dt))
     {
-        $tg = $row[4];
+        $tg = $row[5];
        $k = date("d/m/Y",strtotime($tg));
         echo "<tr>
         <th scope='row'>$row[0]</th>
         <td>$row[1]</td>
         <td>$row[2]</td>
         <td>$row[3]</td>
+        <td>$row[4]</td>
         <td>$k</td>
-        <td><img src='https://img.icons8.com/ios/50/000000/delete--v3.png'/></td>
+        <td>$row[6]</td>
         </tr>";
     }
 }
