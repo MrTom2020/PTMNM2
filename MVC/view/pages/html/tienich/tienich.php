f729091a70c35a43a5c212336c53aaa33006bf1f
MrTom2020 <tomhumchin2@gmail.com>
2021-07-23 07:19:57 +0700

    make it better

--- MVC/view/pages/html/tienich/tienich.php
+++ MVC/view/pages/html/tienich/tienich.php
@@ -11,13 +11,6 @@
 </div>
 <div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
 <div class="animated features-ml">
-<img src="https://img.icons8.com/fluent/71/26e07f/estimate.png"/>
-<h3 class="fourthlabel">Amazing split the money</h3>
-<input type="submit" class="form-control"  name="mt" id="useName" value="Amazing split the money là công cụ chia tiền hữu ích tiền.">
-</div>
-</div>
-<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
-<div class="animated features-ml">
 <img src="https://img.icons8.com/fluent/71/26e07f/rupee-exchange.png"/>
 <h3 class="fourthlabel">Amazing look up</h3>
 <input type="submit" class="form-control"  name="cd" id="cd" value="Amazing money conversion là công cụ chuyển đổi dv tiền.">
@@ -29,13 +22,6 @@
 <img src="https://img.icons8.com/fluent/72/26e07f/microsoft-excel-2019.png"/>
 <h3 class="fourthlabel">Amazing Excel</h3>
 <input type="submit" class="form-control"  name="mt" id="useName" value="Amazing excel sẽ giúp bạn tính toán và xuất file excel.">
-</div>
-</div>
-<div class="col-md-4 col-lg-4 col-xs-6 animated fadeIn">
-<div class="animated features-ml">
-<img src="https://img.icons8.com/fluent/72/26e07f/composing-mail.png"/>
-<h3 class="fourthlabel">Amazing mailbox</h3>
-<input type="submit" class="form-control"  name="mailbox" id="mailbox" value="Amazing mailbox sẽ giúp bạn gửi mail.">
 </div>
 </div>
 </div>
