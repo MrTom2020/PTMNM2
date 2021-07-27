acbfe0b865fb11b32826cdf477732f3f4dc08b28
MrTom2020 <tomhumchin2@gmail.com>
2021-07-27 23:45:30 +0700

    make it better

--- MVC/view/pages/js/test.js
+++ MVC/view/pages/js/test.js
@@ -1,6 +1,6 @@
 var ctxP = document.getElementById("pieChart").getContext('2d');
 var kkkk;
-    $.post("../baocaothuvachi/baocaoct",{un3:mavi},function(data){ kkkk = data; });
+    $.post("../baocaothuvachi/baocaoct",{hi:mavi},function(data){ kkkk = data; });
     var d = [];
     //d = [10,20,30,40,50];
     for($i = 0;$i < 10;$i+=1)
