cb2ef0b1f5c1742131b2149163cf9e1237793886
MrTom2020 <tomhumchin2@gmail.com>
2021-07-28 00:36:11 +0700

    make it better

--- MVC/view/pages/js/test.js
+++ MVC/view/pages/js/test.js
@@ -1,6 +1,6 @@
     var ctxP = document.getElementById("pieChart").getContext('2d');
 var kkkk;
-    kkkk = $("#txtJob").val();
+    kkkk = document.getElementById('txtJob').value;
     //kkkk = $.post("hi").value;
     var d = [];
     //d = [10,20,30,40,50];
