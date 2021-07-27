f2dfc5d9e15ccd538749161b960a1459733011f1
MrTom2020 <tomhumchin2@gmail.com>
2021-07-27 20:27:31 +0700

    make it better

--- MVC/controler/baocaothuvachi.php
+++ MVC/controler/baocaothuvachi.php
@@ -72,12 +72,11 @@
         $tongtien = $this->modelcon->tongtien($ID);
         $tienchi = $this->tongtienchi->tongtienc($ID) * -1;
         $un3 = isset($_POST['un3']) ? $_POST['un3']:"";
-        location.reload();
         for($i = 0;$i < 1;$i++)
         {
             echo $i;
             echo "<div>
-            <canvas id='pieChart' style='max-width: 500px;'></canvas>
+            <canvas id='pieChart' style='max-width: 500px;'>123</canvas>
             </div>";
           echo "<script src='https://quanlychitieu2030.herokuapp.com/MVC/view/pages/js/test.js'></script>";
         }
