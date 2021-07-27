4f9301cb5031d5ce75aa8c967a833fc0a36a3d50
MrTom2020 <tomhumchin2@gmail.com>
2021-07-27 14:32:08 +0700

    make it better

--- MVC/view/pages/js/test.js
+++ MVC/view/pages/js/test.js
@@ -1,10 +1,15 @@
     var ctxP = document.getElementById("pieChart").getContext('2d');
+    var c = Array( );
+    for($i = 0;$i < 10; $i++)
+    {
+        array_push(c,$i);
+    }
     var myPieChart = new Chart(ctxP, {
       type: 'pie',
       data: {
         labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
         datasets: [{
-          data: [300, 50, 100, 40, 120],
+          data: c,
           backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
           hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
         }]
