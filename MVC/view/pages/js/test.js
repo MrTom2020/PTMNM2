bfb7f7f3db9a8f2b5723ebca196f78208b201e8f
MrTom2020 <tomhumchin2@gmail.com>
2021-07-27 14:58:03 +0700

    make it better

--- MVC/view/pages/js/test.js
+++ MVC/view/pages/js/test.js
@@ -4,16 +4,12 @@
     {
         c.push($i);
     }
-    for($ii = 0;$ii < 10;$ii++)
-    {
-        alert(c[$ii]);
-    }
     var myPieChart = new Chart(ctxP, {
       type: 'pie',
       data: {
         labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
         datasets: [{
-          data: [300, 50, 100, 40, 120],
+          data: c,
           backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
           hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
         }]
