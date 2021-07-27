50a120b20d97459e81c73c34fd86b7369ad3dfb3
MrTom2020 <tomhumchin2@gmail.com>
2021-07-27 22:46:22 +0700

    make it better

--- MVC/view/pages/js/test.js
+++ MVC/view/pages/js/test.js
@@ -1,12 +1,12 @@
 var ctxP = document.getElementById("pieChart").getContext('2d');
-    const c = [10,20,30,40,50];
+    const d = [10,20,30,40,50];
    
     var myPieChart = new Chart(ctxP, {
       type: 'pie',
       data: {
         labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
         datasets: [{
-          data: c,
+          data: d,
           backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
           hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
         }]
