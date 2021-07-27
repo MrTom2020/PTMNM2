44286b4b7d4d6f6f9925d20486f827a706d03227
MrTom2020 <tomhumchin2@gmail.com>
2021-07-27 16:53:07 +0700

    make it better

--- MVC/controler/baocaothuvachi.php
+++ MVC/controler/baocaothuvachi.php
@@ -72,7 +72,7 @@
         $tongtien = $this->modelcon->tongtien($ID);
         $tienchi = $this->tongtienchi->tongtienc($ID) * -1;
         $un3 = isset($_POST['un3']) ? $_POST['un3']:"";
-       echo t(1000);
+       echo $this->t(1000);
         if($un3)
         {
             echo "<div>
