d163690d61a0311777d62fa1427b30c23ff73db1
MrTom2020 <tomhumchin2@gmail.com>
2021-07-27 23:22:17 +0700

    make it better

--- MVC/view/pages/js/test.js
+++ MVC/view/pages/js/test.js
@@ -1,7 +1,7 @@
 var ctxP = document.getElementById("pieChart").getContext('2d');
-    const d;
+    const d = [];
     //d = [10,20,30,40,50];
-    for(var i = 0;i < 100;i+=1)
+    for($i = 0;$i < 100;$i+=1)
     {
       d.push($i);
     }
