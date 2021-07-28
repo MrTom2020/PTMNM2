9bdcaaffd7dd0ce2a188cc750a881a4853d9d1fd
MrTom2020 <tomhumchin2@gmail.com>
2021-07-29 00:28:38 +0700

    make it better

--- MVC/controler/baocaothuvachi.php
+++ MVC/controler/baocaothuvachi.php
@@ -89,8 +89,8 @@
         echo "<input type='text'id='txtJob3' name='txtJob3' value='$kkk2'>";
             echo "<div>
             <canvas id='pieChart' style='max-width: 500px;margin-left:-40vw;margin-top:-75vh;'></canvas>
-            <h3>Báo cáo chi tiết của ví $un3</h3>
-            </div>";
+            </div>
+            <h3>Báo cáo chi tiết của ví $un3</h3>";
           echo "<script src='https://quanlychitieu2030.herokuapp.com/MVC/view/pages/js/test.js'></script>";
         
     }
