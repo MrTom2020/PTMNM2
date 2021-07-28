7308cfbe41ffb64424083b48f2ef002de3726fda
MrTom2020 <tomhumchin2@gmail.com>
2021-07-28 08:32:46 +0700

    make it better

--- MVC/view/pages/html/baocaochitiet.php
+++ MVC/view/pages/html/baocaochitiet.php
@@ -1,7 +1,7 @@
 <nav>
         <?php include_once('menu_main/menu.php'); ?>
    </nav>
-   <section style="width:50vw;float:right;">
+   <section style="width:50vw;float:right;height:70vh;">
    <?php 
       $Page = isset($data['Page1']) ? $data['Page1']:"";
       include_once('baocao/'.$Page.'.php');
