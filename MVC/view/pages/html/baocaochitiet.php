82c15057f794e359670ce1cab5e82e8fb0a28b43
MrTom2020 <tomhumchin2@gmail.com>
2021-07-28 08:22:01 +0700

    make it better

---
+++ MVC/view/pages/html/baocaochitiet.php
@@ -0,0 +1,9 @@
+<nav>
+        <?php include_once('menu_main/menu.php'); ?>
+   </nav>
+   <section style="width:50vw;float:right;margin-top:-75vh;">
+   <?php 
+      $Page = isset($data['Page1']) ? $data['Page1']:"";
+      include_once('baocao/'.$Page.'.php');
+       ?>
+   </section>
